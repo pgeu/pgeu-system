06f44d5126cbe217c2276aa898369506dfaa2529
Magnus Hagander <magnus@hagander.net>
2009-09-28 23:06:53 +0200

    Update admin view of conference registrations to show wether it's confirmed or not (and when).

--- postgresqleu/confreg/admin.py
+++ postgresqleu/confreg/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from postgresqleu.confreg.models import *
 
+class ConferenceRegistrationAdmin(admin.ModelAdmin):
+	list_display = ['email', 'conference', 'firstname', 'lastname', 'payconfirmedat', ]
+	ordering = ['-payconfirmedat', 'lastname', 'firstname', ]
+
 admin.site.register(Conference)
 admin.site.register(RegistrationType)
 admin.site.register(ShirtSize)
-admin.site.register(ConferenceRegistration)
+admin.site.register(ConferenceRegistration, ConferenceRegistrationAdmin)
 admin.site.register(PaymentOption)
