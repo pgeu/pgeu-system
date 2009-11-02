4c3388213f55ebd629e873454c270082f3ed0c61
Magnus Hagander <magnus@hagander.net>
2009-11-02 15:00:33 +0100

    Allow saving of forms that have expired regtype on them. Still disallow changing the regtype then, of course.

--- postgresqleu/confreg/forms.py
+++ postgresqleu/confreg/forms.py
@@ -7,11 +7,17 @@ from postgresqleu.confreg.models import *
 class ConferenceRegistrationForm(forms.ModelForm):
 	def clean_regtype(self):
 		newval = self.cleaned_data.get('regtype')
+		if self.instance and newval == self.instance.regtype:
+			# Registration type not changed, so it's ok to save
+			# (we don't want to prohibit other edits for somebody who has
+			#  an already-made registration with an expired registration type)
+			return newval
+
 		if newval and not newval.active:
 			raise forms.ValidationError('Registration type "%s" is currently not available.' % newval)
+
 		if self.instance and self.instance.payconfirmedat:
-			if newval != self.instance.regtype:
-				raise forms.ValidationError('You cannot change type of registration once your payment has been confirmed!')
+			raise forms.ValidationError('You cannot change type of registration once your payment has been confirmed!')
 
 		return newval
 
