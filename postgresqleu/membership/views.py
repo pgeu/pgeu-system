eb86d0632d9f60c8a610ae98a2587a06a49da6f0
Magnus Hagander <magnus@hagander.net>
2010-02-22 17:07:10 +0100

    Include country a member lives in

--- postgresqleu/membership/views.py
+++ postgresqleu/membership/views.py
@@ -65,7 +65,7 @@ def home(request):
 
 
 def userlist(request):
-	members = Member.objects.filter(listed=True, paiduntil__gt=datetime.now()).order_by('fullname')
+	members = Member.objects.select_related('country').filter(listed=True, paiduntil__gt=datetime.now()).order_by('fullname')
 	return render_to_response('community/userlist.html', {
 		'members': members,
 	}, context_instance=RequestContext(request))
