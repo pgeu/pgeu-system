b651f294660aafc15ef74031f04677d703e8c992
Magnus Hagander <magnus@hagander.net>
2010-10-20 14:58:03 +0200

    Add simple status reporting tool

---
+++ tools/confreg/confreport.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+#
+# Tool to generate a report of conference registrations and
+# additional options.
+#
+import sys
+import os
+import psycopg2
+import psycopg2.extensions
+import datetime
+from StringIO import StringIO
+
+
+def Usage():
+	print "Usage: confreport.py <connectionstring> <conferenceshortname>"
+	sys.exit(1)
+
+if __name__ == "__main__":
+	if len(sys.argv) != 3:
+		Usage()
+	connstr = sys.argv[1]
+	confname = sys.argv[2]
+
+	psycopg2.extensions.register_type(psycopg2.extensions.UNICODE)
+	db = psycopg2.connect(connstr)
+	curs = db.cursor()
+	curs.execute("SELECT id,conferencename FROM confreg_conference WHERE urlname=%(url)s", {'url': confname})
+	try:
+		r = curs.fetchall()[0]
+		confid = r[0]
+		confname = r[1]
+	except:
+		print "Could not find conference in database!"
+		sys.exit(1)
+
+	s = StringIO()
+	s.writelines("Status for %s per %s\n\n\n" % (confname, datetime.datetime.now()))
+	
+	curs.execute("""
+SELECT regtype,
+       SUM(CASE WHEN payconfirmedat IS NOT NULL THEN 1 ELSE 0 END) AS paid,
+       SUM(CASE WHEN payconfirmedat IS NULL THEN 1 ELSE 0 END) AS nonpaid
+FROM confreg_conferenceregistration cr
+INNER JOIN confreg_registrationtype rt ON cr.regtype_id=rt.id
+WHERE cr.conference_id=%(id)s
+GROUP BY regtype
+ORDER BY 1,2
+""", { 'id': confid })
+	s.writelines("Registrations per type\n")
+	s.writelines("----------------------\n")
+	s.writelines("%-50s %13s %13s\n" % ('Type', 'Confirmed', 'Unconfirmed'))
+	s.writelines(["%-50s %13s %13s\n" % r for r in curs.fetchall()])
+
+	curs.execute("""
+SELECT name,
+       SUM(CASE WHEN payconfirmedat IS NOT NULL THEN 1 ELSE 0 END) AS paid,
+       SUM(CASE WHEN payconfirmedat IS NULL THEN 1 ELSE 0 END) AS nonpaid
+FROM confreg_conferenceadditionaloption cao
+INNER JOIN confreg_conferenceregistration_additionaloptions cr_ao ON cao.id=cr_ao.conferenceadditionaloption_id
+INNER JOIN confreg_conferenceregistration cr ON cr.id=cr_ao.conferenceregistration_id
+WHERE cao.conference_id=%(id)s
+GROUP BY name
+ORDER BY 1
+""", { 'id': confid })
+	s.writelines("\n")
+	s.writelines("Additional options\n")
+	s.writelines("------------------\n")
+	s.writelines("%-50s %13s %13s\n" % ('Type', 'Confirmed', 'Unconfirmed'))
+	s.writelines(["%-50s %13s %13s\n" % (r[0][:50],r[1],r[2]) for r in curs.fetchall()])
+
+	print s.getvalue()
