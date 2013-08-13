7758b667992d70667b9e03367b9a3465d38e3857
Magnus Hagander <magnus@hagander.net>
2013-08-13 22:36:08 +0200

    Prevent generation of bulk invoices for a single user
    There is no point and it complicates all things. Bulk payment should
    require at least 2 users.

--- postgresqleu/confreg/forms.py
+++ postgresqleu/confreg/forms.py
@@ -313,3 +313,10 @@ class BulkRegistrationForm(forms.Form):
 	recipient_name = forms.CharField(required=True, max_length=100,label='Invoice recipient name')
 	recipient_address = forms.CharField(required=True, max_length=100, label='Invoice recipient address', widget=forms.Textarea)
 	email_list = forms.CharField(required=True, label='Emails to pay for', widget=forms.Textarea)
+
+	def clean_email_list(self):
+		email_list = self.cleaned_data.get('email_list')
+		emails = [e for e in email_list.splitlines(False) if e]
+		if len(emails) < 2:
+			raise ValidationError('Bulk payments can only be done for 2 or more emails')
+		return email_list
