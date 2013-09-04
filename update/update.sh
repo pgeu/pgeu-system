c505ef334f2beab4209bf815dae06923dc0f1fca
Magnus Hagander <magnus@hagander.net>
2013-09-04 20:08:52 +0200

    Create a pkill command that doesn't kill itself

--- update/update.sh
+++ update/update.sh
@@ -31,7 +31,7 @@ fi
 
 # Cause reload
 echo Reloading website due to updates
-sudo pkill -f postgresqleu/manage.py
+sudo pkill -f ^python.*postgresqleu/manage.py
 
 # Update the file listing the latest update
 mv -f /tmp/pgeu.update update/lastupdate
