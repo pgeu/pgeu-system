870b6715358578a46e347fbec404c93921fa9ef5
Magnus Hagander <magnus@hagander.net>
2013-09-10 16:42:01 +0200

    Show confirmed field in list view of notifications

--- postgresqleu/adyen/admin.py
+++ postgresqleu/adyen/admin.py
@@ -7,7 +7,7 @@ class RawNotificationAdmin(admin.ModelAdmin):
 	list_display = ('dat', 'confirmed',)
 
 class NotificationAdmin(admin.ModelAdmin):
-	list_display = ('receivedat', 'eventDate', 'eventCode', 'live', 'success', 'pspReference', )
+	list_display = ('receivedat', 'eventDate', 'eventCode', 'live', 'success', 'confirmed', 'pspReference', )
 
 class ReportAdmin(admin.ModelAdmin):
 	list_display = ('receivedat', 'processedat', 'url',)
