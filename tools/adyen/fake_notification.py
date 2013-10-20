c82e81ebc3f58942bee8a635a6ee0eebdd8be1c4
Magnus Hagander <magnus@hagander.net>
2013-10-20 16:31:28 +0200

    Add tool to fake adyen notifications, for easier local testing

---
+++ tools/adyen/fake_notification.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+#
+# This script fakes an Adyen notification of different kinds, including
+# sending it with correct passwords and such things.
+# Clearly - use with care!
+#
+#
+# Copyright (C) 2013, PostgreSQL Europe
+#
+
+import os
+import sys
+import base64
+import urllib
+import urllib2
+
+# We import the django stuff just so we can get our hands on the
+# settings which have the notification user/password in it.
+from django.core.management import setup_environ
+sys.path.append(os.path.join(os.path.abspath(os.path.dirname(sys.argv[0])), '../../postgresqleu'))
+import settings
+setup_environ(settings)
+
+
+if __name__=="__main__":
+	body = ""
+	if len(sys.argv)==3 and sys.argv[1] == "raw":
+		body = raw_input('Enter the http POST data as a single string: ')
+	elif len(sys.argv)==3 and sys.argv[1] == "prompt":
+		fields = ('merchantAccountCode', 'pspReference', 'merchantReference', 'originalReference', 'eventDate', 'eventCode', 'paymentMethod', 'live', 'success', 'value', 'currency', 'reason', )
+		vals = {}
+		for f in fields:
+			vals[f] = raw_input('%s:' % f)
+		body = urllib.urlencode(vals)
+	else:
+		print "Usage: fake_notification.py <raw|prompt> <baseurl>"
+		sys.exit(1)
+
+	print "'%s'\n" % body
+	base = sys.argv[2]
+	url = "%s/p/adyen_notify/" % base
+	while True:
+		r = raw_input("Are you sure you want to send this notification to %s ?" % url)
+		if r.lower().startswith('y'):
+			break
+		if r.lower().startswith('n'):
+			sys.exit(0)
+
+	req = urllib2.Request(url, body)
+	req.add_header("Authorization", "Basic %s" % (
+		base64.encodestring("%s:%s" % (
+			settings.ADYEN_NOTIFY_USER,
+			settings.ADYEN_NOTIFY_PASSWORD,
+		)).replace('\n', '')))
+	try:
+		resp = urllib2.urlopen(req)
+		print resp.read()
+	except urllib2.HTTPError, e:
+		print "Error %s from server" % e.code
+		with open('/tmp/fake_notify_error.txt', 'w') as f:
+			f.write(e.read())
+		print "Content written to /tmp/fake_notify_error.txt"
+
