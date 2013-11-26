34fff9934eb7b219793caa3d8bfa0afc53d3283a
Magnus Hagander <magnus@hagander.net>
2013-11-26 12:41:24 +0100

    Add tool to backfill paypal fees into the database
    Used to backfill fee information for all transactions where we haven't
    collected that information before. Once this info is on all transactions,
    this script will no longer be useful...

---
+++ tools/paypal/oneoff/backfill_paypal_fees.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+# Backfill paypal fees into the database, from the dates before
+# we were tracking them live.
+
+import os
+import sys
+import ConfigParser
+from decimal import Decimal
+
+sys.path.append('..')
+from paypal import PaypalAPI
+
+# Set up to run in django environment
+from django.core.management import setup_environ
+sys.path.append(os.path.join(os.path.abspath(os.path.dirname(sys.argv[0])), '../../../postgresqleu'))
+import settings
+setup_environ(settings)
+
+from django.db import transaction
+
+from postgresqleu.paypal.models import TransactionInfo
+
+cfg = ConfigParser.ConfigParser()
+cfg.read('../paypal.ini')
+
+if __name__ == "__main__":
+	api = PaypalAPI(cfg.get('postgresqleu', 'user'), cfg.get('postgresqleu', 'apipass'), cfg.get('postgresqleu', 'apisig'), 0)
+
+	for ti in TransactionInfo.objects.filter(fee__isnull=True).order_by('timestamp'):
+		with transaction.commit_on_success():
+			print ti.paypaltransid
+			info = api.get_transaction_details(ti.paypaltransid)
+			if Decimal(info['AMT'][0]) != ti.amount:
+				print "%s: Unmatched amounts. Db has %s, paypal has %s." % (ti.paypaltransid, ti.amount, info['AMT'][0])
+				sys.exit(1)
+			# Amounts match, get the fee
+			ti.fee = Decimal(info['FEEAMT'][0])
+			print "%s: Amount %s, fee %s (%s%%)" % (ti.paypaltransid, ti.amount, ti.fee, 100*ti.fee/ti.amount)
+			ti.save()
