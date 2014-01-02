e1e2929b3ee989a9011d3f3b7b838aee46c50b1c
Magnus Hagander <magnus@hagander.net>
2014-01-02 15:18:59 +0100

    Fix date constraint in ledger report
    Should be <= not < when comparing to end date.

--- postgresqleu/accounting/views.py
+++ postgresqleu/accounting/views.py
@@ -320,7 +320,7 @@ def report(request, year, reporttype):
 		# XXX: consider perhaps including the in and out balance as well.
 
 		# Yup, the django ORM fails again - no window aggregates
-		sql = "SELECT a.num as accountnum, a.name as accountname, sum(case when i.amount>0 then i.amount else 0 end) over w1 as totaldebit, sum(case when i.amount < 0 then -i.amount else 0 end) over w1 as totalcredit, e.seq as entryseq, e.date, i.description, case when i.amount > 0 then i.amount else 0 end as debit, case when i.amount < 0 then -i.amount else 0 end as credit, o.name as object FROM accounting_journalitem i INNER JOIN accounting_account a ON i.account_id=a.num INNER JOIN accounting_journalentry e ON i.journal_id=e.id LEFT JOIN accounting_object o ON i.object_id=o.id WHERE e.year_id=%(year)s AND e.closed AND e.date<%(enddate)s"
+		sql = "SELECT a.num as accountnum, a.name as accountname, sum(case when i.amount>0 then i.amount else 0 end) over w1 as totaldebit, sum(case when i.amount < 0 then -i.amount else 0 end) over w1 as totalcredit, e.seq as entryseq, e.date, i.description, case when i.amount > 0 then i.amount else 0 end as debit, case when i.amount < 0 then -i.amount else 0 end as credit, o.name as object FROM accounting_journalitem i INNER JOIN accounting_account a ON i.account_id=a.num INNER JOIN accounting_journalentry e ON i.journal_id=e.id LEFT JOIN accounting_object o ON i.object_id=o.id WHERE e.year_id=%(year)s AND e.closed AND e.date<=%(enddate)s"
 		params = {
 			'year': year.year,
 			'enddate': enddate,
