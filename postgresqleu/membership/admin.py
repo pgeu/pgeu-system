cee328923518aa931057fa8ab73fae6ce7246f8a
Magnus Hagander <magnus@hagander.net>
2014-01-11 21:50:27 +0100

    Add a filter to view only active or inactive members

--- postgresqleu/membership/admin.py
+++ postgresqleu/membership/admin.py
@@ -1,10 +1,31 @@
 from django.contrib import admin
+from django.db.models import Q
+
+from datetime import datetime
 
 from models import Member, MemberLog
+
+class ActiveMemberFilter(admin.SimpleListFilter):
+	title = 'Active'
+	parameter_name = 'isactive'
+
+	def lookups(self, request, model_admin):
+		return (
+			('Yes', 'Yes'),
+			('No', 'No'),
+		)
+
+	def queryset(self, request, queryset):
+		if self.value() == 'Yes':
+			return queryset.filter(paiduntil__gte=datetime.now())
+		if self.value() == 'No':
+			return queryset.filter(Q(paiduntil__lt=datetime.now()) | Q(paiduntil__isnull=True))
+		return queryset
 
 class MemberAdmin(admin.ModelAdmin):
 	list_display = ('user', 'fullname', 'country', 'membersince', 'paiduntil', )
 	ordering = ('user',)
+	list_filter = (ActiveMemberFilter, )
 
 	def change_view(self, request, object_id, extra_context=None):
 		member = Member(pk=object_id)
