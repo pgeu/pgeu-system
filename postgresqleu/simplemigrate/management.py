2862fea0c748ab0f134d047302600c12d5260a6f
Magnus Hagander <magnus@hagander.net>
2014-07-12 16:22:30 +0200

    Try to fix sqlupdates for empty installs
    Previously, undocumented manual steps were required to initialize
    the _appscripts table. Try to work around that :)
    
    1. For entirely new installs, models that have any SQL update scripts
       will automatically get set to a position past the latest update.
    2. For apps that have no update script, we now insert an empty record
       in _appscripts, as this makes it possible to apply scripts that
       appear in the future, without giving up the automatic skipping of
       scripts for completely new installations.

--- postgresqleu/simplemigrate/management.py
+++ postgresqleu/simplemigrate/management.py
@@ -5,6 +5,12 @@ import os
 from glob import glob
 import re
 
+def _get_app_files(appname):
+	for fn in sorted(glob('%s/sqlupdate/*.sql' % appname)):
+		m = re.search('/(\d+)(-[^/]+)?\.sql$', fn)
+		if not m: continue
+		yield (fn, int(m.groups(1)[0]))
+
 def process_sql_scripts(app, created_models, verbosity=2, **kwargs):
 	if not app.__name__.startswith('postgresqleu.'): return
 
@@ -13,23 +19,32 @@ def process_sql_scripts(app, created_models, verbosity=2, **kwargs):
 	if namepieces[2] != 'models': return
 	appname = namepieces[1]
 
-	if not os.path.isdir('%s/sqlupdate' % appname): return
-
 	with transaction.commit_on_success():
+		appfiles = list(_get_app_files(appname))
 		curs = connection.cursor()
 		curs.execute("SELECT ver FROM _appscripts WHERE app=%s", [appname,])
 		rows = curs.fetchall()
 		if len(rows) == 0:
 			# App not inserted yet, so do that
-			curs.execute("INSERT INTO _appscripts (app,ver) VALUES (%s,%s)", [appname, 0])
-			ver = 0
+			if len(appfiles):
+				# One or more appfiles exist, so we need to bypass them. This is the normal
+				# usecase when doing a completely new deployment.
+				(lastfile, lastver) = appfiles[-1]
+				print "Initializing SQL update scripts for %s, setting last version to %s" % (appname, lastver)
+				curs.execute("INSERT INTO _appscripts (app,ver) VALUES (%s,%s)", [appname, lastver])
+				ver = lastver
+			else:
+				# No appfiles exist, meaning that this model has no updates. We do, however,
+				# insert an empty record for it specifically so that we can handle the *next* time an
+				# update shows up. Once the file shows up it will then be applied, whereas a completely
+				# new deployment will have nothing and still bypass it.
+				print "Initializing empty SQL update scripts for %s" % appname
+				curs.execute("INSERT INTO _appscripts (app,ver) VALUES (%s,%s)", [appname, 0])
+				ver = 0
 		else:
 			ver = rows[0][0]
 
-		for fn in sorted(glob('%s/sqlupdate/*.sql' % appname)):
-			m = re.search('/(\d+)(-[^/]+)?\.sql$', fn)
-			if not m: continue
-			filever = int(m.groups(1)[0])
+		for (fn, filever) in appfiles:
 			if ver >= filever:
 				print "SQL script %s already applied." % fn
 				continue
