pgeu/pgeu-system

commit 8e1105c97a3bcbda96cad0b304e7250e5e3b2039

Fix rendering of vouchers used by people with non-ascii characters in name
Only for sponsor usage overview page, other views were fine.

postgresqleu/confsponsor/benefitclasses/entryvouchers.py
@@ -84,6 +84,6 @@ class EntryVouchers(BaseBenefit):
 		s.write("<p>%s vouchers were generated, %s have been used.</p>" % (generated, used))
 		s.write("<table><tr><th>Voucher code</th><th>Used by</th><th>Used at</th></tr>")
 		for v in vouchers:
-			s.write("<tr><td><code>%s</code></td><td>%s</td><td>%s</td></tr>" % (v.vouchervalue, v.user and v.user.fullname or '', v.usedate and v.usedate or ''))
+			s.write("<tr><td><code>{0}</code></td><td>{1}</td><td>{2}</td></tr>".format(v.vouchervalue, v.user and v.user.fullname.encode('utf8'), v.usedate and v.usedate or ''))
 		s.write("</table>")
 		return s.getvalue()

commit bdbf7ab257f03b49bafe347071bd21b9a9bcc027

Fix used-by display for unused vouchers
Accidentally broken in 8e1105c97a3bcbda96cad0b304e7250e5e3b2039

postgresqleu/confsponsor/benefitclasses/entryvouchers.py
@@ -100,6 +100,6 @@ class EntryVouchers(BaseBenefit):
 		s.write("<p>%s vouchers were generated, %s have been used.</p>" % (generated, used))
 		s.write("<table><tr><th>Voucher code</th><th>Used by</th><th>Used at</th></tr>")
 		for v in vouchers:
-			s.write("<tr><td><code>{0}</code></td><td>{1}</td><td>{2}</td></tr>".format(v.vouchervalue, v.user and v.user.fullname.encode('utf8'), v.usedate and v.usedate or ''))
+			s.write("<tr><td><code>{0}</code></td><td>{1}</td><td>{2}</td></tr>".format(v.vouchervalue, v.user and v.user.fullname.encode('utf8') or '', v.usedate and v.usedate or ''))
 		s.write("</table>")
 		return s.getvalue()