2cb1ee29260d94abe366803a6ce8030933573723
Magnus Hagander <magnus@hagander.net>
2015-05-17 14:53:11 +0200

    Add validators for validating forms that have date fields in them

--- postgresqleu/util/validators.py
+++ postgresqleu/util/validators.py
@@ -3,3 +3,20 @@ from django.core.exceptions import ValidationError
 def validate_lowercase(value):
 	if value != value.lower():
 		raise ValidationError("This field must be lowercase only")
+
+
+class BeforeValidator(object):
+	def __init__(self, beforedate):
+		self.beforedate = beforedate
+
+	def __call__(self, value):
+		if value >= self.beforedate:
+			raise ValidationError("Ensure this date is before {0}".format(self.beforedate))
+
+class AfterValidator(object):
+	def __init__(self, afterdate):
+		self.afterdate = afterdate
+
+	def __call__(self, value):
+		if value <= self.afterdate:
+			raise ValidationError("Ensure this date is after {0}".format(self.afterdate))
