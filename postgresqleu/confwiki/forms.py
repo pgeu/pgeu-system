08bbc535b899d2a685b1e5678a38f2ac841d58d7
Magnus Hagander <magnus@hagander.net>
2015-08-27 14:37:57 +0200

    Use selectize on wiki admin edit form

--- postgresqleu/confwiki/forms.py
+++ postgresqleu/confwiki/forms.py
@@ -12,10 +12,13 @@ class WikipageAdminEditForm(forms.ModelForm):
 	def __init__(self, *args, **kwargs):
 		super(WikipageAdminEditForm, self).__init__(*args, **kwargs)
 		self.fields['author'].queryset = ConferenceRegistration.objects.filter(conference=self.instance.conference)
+		self.fields['author'].label_from_instance = lambda r: "{0} <{1}>".format(r.fullname, r.email)
 		self.fields['viewer_regtype'].queryset = RegistrationType.objects.filter(conference=self.instance.conference)
 		self.fields['editor_regtype'].queryset = RegistrationType.objects.filter(conference=self.instance.conference)
 		self.fields['viewer_attendee'].queryset = ConferenceRegistration.objects.filter(conference=self.instance.conference)
+		self.fields['viewer_attendee'].label_from_instance = lambda r: "{0} <{1}>".format(r.fullname, r.email)
 		self.fields['editor_attendee'].queryset = ConferenceRegistration.objects.filter(conference=self.instance.conference)
+		self.fields['editor_attendee'].label_from_instance = lambda r: "{0} <{1}>".format(r.fullname, r.email)
 
 	class Meta:
 		model = Wikipage
