3fc9c1ed10f9b46ca5f444fbd2c5270c8157fa2e
Magnus Hagander <magnus@hagander.net>
2016-01-07 15:19:24 +0100

    Fully qualify names of payment providers
    This REQUIRES that the following SQL is run before the upgrade:
    
    UPDATE invoices_invoicepaymentmethod SET
      classname='postgresqleu.'||classname WHERE classname like 'util.payment%';
    
    Without this, all payment providers will be duplicated!

--- postgresqleu/invoices/management.py
+++ postgresqleu/invoices/management.py
@@ -11,27 +11,27 @@ def add_invoice_handler(app, model, name, classname=None):
 
 def register_invoice_payment_methods(app, created_models, verbosity=2, **kwargs):
 	if app.__name__ == 'postgresqleu.invoices.models':
-		(p, created) = InvoicePaymentMethod.objects.get_or_create(classname='util.payment.paypal.Paypal', defaults={'name':'Paypal or creditcard', 'sortkey':100, 'internaldescription': 'Paypal'})
+		(p, created) = InvoicePaymentMethod.objects.get_or_create(classname='postgresqleu.util.payment.paypal.Paypal', defaults={'name':'Paypal or creditcard', 'sortkey':100, 'internaldescription': 'Paypal'})
 		if created:
 			p.save()
 			print "Added payment processor for paypal"
 
-		(p, created) = InvoicePaymentMethod.objects.get_or_create(classname='util.payment.banktransfer.Banktransfer', defaults={'name':'Bank transfer', 'sortkey':200, 'auto':False, 'internaldescription': 'Manual bank transfer'})
+		(p, created) = InvoicePaymentMethod.objects.get_or_create(classname='postgresqleu.util.payment.banktransfer.Banktransfer', defaults={'name':'Bank transfer', 'sortkey':200, 'auto':False, 'internaldescription': 'Manual bank transfer'})
 		if created:
 			p.save()
 			print "Added payment processor for bank transfer"
 
-		(p, created) = InvoicePaymentMethod.objects.get_or_create(classname='util.payment.adyen.AdyenCreditcard', defaults={'name':'Credit card', 'sortkey':50, 'auto':True, 'internaldescription': 'Adyen creditcard'})
+		(p, created) = InvoicePaymentMethod.objects.get_or_create(classname='postgresqleu.util.payment.adyen.AdyenCreditcard', defaults={'name':'Credit card', 'sortkey':50, 'auto':True, 'internaldescription': 'Adyen creditcard'})
 		if created:
 			p.save()
 			print "Added payment processor for adyen creditcard"
 
-		(p, created) = InvoicePaymentMethod.objects.get_or_create(classname='util.payment.adyen.AdyenBanktransfer', defaults={'name':'Direct bank transfer', 'sortkey':75, 'auto':False, 'internaldescription': 'Adyen managed bank transfer'})
+		(p, created) = InvoicePaymentMethod.objects.get_or_create(classname='postgresqleu.util.payment.adyen.AdyenBanktransfer', defaults={'name':'Direct bank transfer', 'sortkey':75, 'auto':False, 'internaldescription': 'Adyen managed bank transfer'})
 		if created:
 			p.save()
 			print "Added payment processor for adyen bank transfer"
 
-		(p, created) = InvoicePaymentMethod.objects.get_or_create(classname='util.payment.dummy.DummyPayment', defaults={'name':'Dummy', 'sortkey':999, 'auto':False, 'active':False})
+		(p, created) = InvoicePaymentMethod.objects.get_or_create(classname='postgresqleu.util.payment.dummy.DummyPayment', defaults={'name':'Dummy', 'sortkey':999, 'auto':False, 'active':False})
 		if created:
 			p.save()
 			print "Added payment processor for dummy payment"
