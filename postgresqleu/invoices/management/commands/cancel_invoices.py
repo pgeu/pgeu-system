7466589e3cf2af4071741ff016e8e7726e65e6a8
Magnus Hagander <magnus@hagander.net>
2016-01-08 13:57:15 +0100

    Move invoice cronjobs to django management commands

--- postgresqleu/invoices/management/commands/cancel_invoices.py
+++ postgresqleu/invoices/management/commands/cancel_invoices.py
@@ -4,36 +4,28 @@
 #
 # Copyright (C) 2015, PostgreSQL Europe
 #
+from django.core.management.base import BaseCommand, CommandError
+from django.db import transaction
 
-import os
-import sys
 from datetime import datetime
-
-# Set up to run in django environment
-from django.core.management import setup_environ
-sys.path.append(os.path.join(os.path.abspath(os.path.dirname(sys.argv[0])), '../../postgresqleu'))
-import settings
-setup_environ(settings)
-
-from django.db import transaction, connection
-
 
 from postgresqleu.invoices.models import Invoice
 from postgresqleu.invoices.util import InvoiceManager
 
 
-if __name__ == "__main__":
-	with transaction.commit_on_success():
+class Command(BaseCommand):
+	help = 'Cancel invoices that have passed their auto-cancel time'
+
+	@transaction.atomic
+	def handle(self, *args, **options):
 		invoices = Invoice.objects.filter(finalized=True, deleted=False, refunded=False, paidat__isnull=True, canceltime__lt=datetime.now())
 
 		manager = InvoiceManager()
 
 		for invoice in invoices:
-			print "Canceling invoice %s, expired" % invoice.id
+			self.stdout.write("Canceling invoice {0}, expired".format(invoice.id))
 
 			# The manager will automatically cancel any registrations etc,
 			# as well as send an email to the user.
 			manager.cancel_invoice(invoice,
 								   "Invoice passed automatic cancel time {0}".format(invoice.canceltime))
-
-	connection.close()
--- postgresqleu/invoices/management/commands/send_invoice_reminders.py
+++ postgresqleu/invoices/management/commands/send_invoice_reminders.py
@@ -1,29 +1,21 @@
-#!/usr/bin/env python
-#
 # Send invoice reminders.
 #
 # Copyright (C) 2015, PostgreSQL Europe
 #
 
-import os
-import sys
+from django.core.management.base import BaseCommand, CommandError
+from django.db import transaction
+
 from datetime import datetime, timedelta
-
-# Set up to run in django environment
-from django.core.management import setup_environ
-sys.path.append(os.path.join(os.path.abspath(os.path.dirname(sys.argv[0])), '../../postgresqleu'))
-import settings
-setup_environ(settings)
-
-from django.db import transaction, connection
-
 
 from postgresqleu.invoices.models import Invoice
 from postgresqleu.invoices.util import InvoiceWrapper
 
+class Command(BaseCommand):
+	help = 'Send invoice reminders'
 
-if __name__ == "__main__":
-	with transaction.commit_on_success():
+	@transaction.atomic
+	def handle(self, *args, **options):
 		# We send reminder automatically when an invoice is 1 day overdue.
 		# We never send a second reminder, that is done manually.
 		invoices = Invoice.objects.filter(finalized=True, deleted=False, refunded=False, paidat__isnull=True, remindersent__isnull=True, duedate__lt=datetime.now() - timedelta(days=1))
@@ -32,6 +24,4 @@ if __name__ == "__main__":
 			wrapper.email_reminder()
 			invoice.remindersent=datetime.now()
 			invoice.save()
-			print "Sent invoice reminder for #%s - %s" % (invoice.id, invoice.title)
-
-	connection.close()
+			self.stdout.write("Sent invoice reminder for #{0} - {1}".format(invoice.id, invoice.title))
