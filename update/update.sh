a881f5bbeb3c3b66c3db36791dbe3347e05427ae
Magnus Hagander <magnus@hagander.net>
2016-01-09 13:04:31 +0100

    Great simplification of the update script

--- update/update.sh
+++ update/update.sh
@@ -3,13 +3,8 @@
 #
 # The idea:
 #  * git-pull the repository
-#  * if the repository has changed, kill the django processes causing a restart
+#  * uwsgi will automatically restart the application as needed
 #
-# Would be even better if we could touch it only after actual code files have changed,
-# but this will do fine for now.
-
-# This is where git lives on freebsd at least
-PATH=$PATH:/usr/local/bin
 
 # Get to our root directory
 UPDDIR=$(dirname $0)
@@ -17,21 +12,6 @@ cd $UPDDIR/..
 
 # Pull changes from the it repo
 git pull -q|grep -v "Already up-to-date"
-
-# Figure out if something changed
-git log -n1 --pretty=oneline > /tmp/pgeu.update
-if [ -f "update/lastupdate" ]; then
-   cmp update/lastupdate /tmp/pgeu.update
-   if [ "$?" == "0" ]; then
-      # No change, so don't reload
-      rm -f /tmp/pgeu.update
-      exit
-   fi
-fi
-
-# Cause reload
-echo Reloading website due to updates
-sudo pkill -f ^python.*postgresqleu/manage.py
 
 # Update the file listing the latest update
 mv -f /tmp/pgeu.update update/lastupdate
