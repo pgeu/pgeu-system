1867cee7e89efa4bc2e9a6bf01dab5d71a6a40a8
Magnus Hagander <magnus@hagander.net>
2016-01-15 14:45:17 +0100

    Manually set up payment processing entries
    It's a lot less risky than to do it at every single sync/migrate,
    as we've seen when disabled processors came back. And it's a task
    that's only run when a new payment processor shows up, which is extremely
    seldom.

--- postgresqleu/invoices/management.py
+++ postgresqleu/invoices/management.py
@@ -8,33 +8,3 @@ def add_invoice_handler(app, model, name, classname=None):
 		if created:
 			i.save()
 			print "Added invoice processor for %s" % model
-
-def register_invoice_payment_methods(app, created_models, verbosity=2, **kwargs):
-	if app.__name__ == 'postgresqleu.invoices.models':
-		(p, created) = InvoicePaymentMethod.objects.get_or_create(classname='postgresqleu.util.payment.paypal.Paypal', defaults={'name':'Paypal or creditcard', 'sortkey':100, 'internaldescription': 'Paypal'})
-		if created:
-			p.save()
-			print "Added payment processor for paypal"
-
-		(p, created) = InvoicePaymentMethod.objects.get_or_create(classname='postgresqleu.util.payment.banktransfer.Banktransfer', defaults={'name':'Bank transfer', 'sortkey':200, 'auto':False, 'internaldescription': 'Manual bank transfer'})
-		if created:
-			p.save()
-			print "Added payment processor for bank transfer"
-
-		(p, created) = InvoicePaymentMethod.objects.get_or_create(classname='postgresqleu.util.payment.adyen.AdyenCreditcard', defaults={'name':'Credit card', 'sortkey':50, 'auto':True, 'internaldescription': 'Adyen creditcard'})
-		if created:
-			p.save()
-			print "Added payment processor for adyen creditcard"
-
-		(p, created) = InvoicePaymentMethod.objects.get_or_create(classname='postgresqleu.util.payment.adyen.AdyenBanktransfer', defaults={'name':'Direct bank transfer', 'sortkey':75, 'auto':False, 'internaldescription': 'Adyen managed bank transfer'})
-		if created:
-			p.save()
-			print "Added payment processor for adyen bank transfer"
-
-		(p, created) = InvoicePaymentMethod.objects.get_or_create(classname='postgresqleu.util.payment.dummy.DummyPayment', defaults={'name':'Dummy', 'sortkey':999, 'auto':False, 'active':False})
-		if created:
-			p.save()
-			print "Added payment processor for dummy payment"
-
-
-signals.post_syncdb.connect(register_invoice_payment_methods)
---
+++ postgresqleu/invoices/management/commands/setup_payment_providers.py
@@ -0,0 +1,45 @@
+# Set up all available payment providers
+#
+# Copyright (C) 2016, PostgreSQL Europe
+#
+
+from django.core.management.base import BaseCommand, CommandError
+from django.db import transaction, connection
+
+from postgresqleu.invoices.models import InvoicePaymentMethod
+
+class Command(BaseCommand):
+	help = 'Set up payment providers'
+
+	@transaction.atomic
+	def handle(self, *args, **options):
+		# Handle old names if they are there
+		curs = connection.cursor()
+		curs.execute("UPDATE invoices_invoicepaymentmethod SET classname='postgresqleu.'||classname WHERE classname LIKE 'util.payment%' RETURNING classname")
+		for c, in curs.fetchall():
+			self.stdout.write("Updated classname for {0}.".format(self.style.WARNING(c)))
+
+		# Create the ones that don't exist if any
+		(p, created) = InvoicePaymentMethod.objects.get_or_create(classname='postgresqleu.util.payment.paypal.Paypal', defaults={'name':'Paypal or creditcard', 'sortkey':100, 'auto': False, 'internaldescription': 'Paypal', 'active': False})
+		if created:
+			self.stdout.write("Created payment method Paypal ({0})".format(self.style.WARNING("disabled")))
+
+		(p, created) = InvoicePaymentMethod.objects.get_or_create(classname='postgresqleu.util.payment.banktransfer.Banktransfer', defaults={'name':'Bank transfer', 'sortkey':200, 'auto':False, 'internaldescription': 'Manual bank transfer', 'active': False})
+		if created:
+			self.stdout.write("Created payment method Manual bank transfer ({0})".format(self.style.WARNING("disabled")))
+
+		(p, created) = InvoicePaymentMethod.objects.get_or_create(classname='postgresqleu.util.payment.adyen.AdyenCreditcard', defaults={'name':'Credit card', 'sortkey':50, 'auto':False, 'internaldescription': 'Adyen creditcard', 'active': False})
+		if created:
+			self.stdout.write("Created payment method Adyen creditcard ({0})".format(self.style.WARNING("disabled")))
+
+		(p, created) = InvoicePaymentMethod.objects.get_or_create(classname='postgresqleu.util.payment.adyen.AdyenBanktransfer', defaults={'name':'Direct bank transfer', 'sortkey':75, 'auto':False, 'internaldescription': 'Adyen managed bank transfer', 'active': False})
+		if created:
+			self.stdout.write("Created payment method Adyen bank transfer ({0})".format(self.style.WARNING("disabled")))
+
+		(p, created) = InvoicePaymentMethod.objects.get_or_create(classname='postgresqleu.util.payment.dummy.DummyPayment', defaults={'name':'Dummy', 'sortkey':999, 'auto':False, 'active':False})
+		if created:
+			self.stdout.write("Created payment method Dummy ({0})".format(self.style.WARNING("disabled")))
+
+		(p, created) = InvoicePaymentMethod.objects.get_or_create(classname='postgresqleu.util.payment.braintree.BrainTree', defaults={'name':'Credit card', 'sortkey':51, 'auto':False, 'internaldescription': 'Braintree managed creditcard', 'active': False})
+		if created:
+			self.stdout.write("Created payment method Braintree creditcard ({0})".format(self.style.WARNING("disabled")))
