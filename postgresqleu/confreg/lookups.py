d7b7aea5ca9857806f9643901a3dbc099eef7c90
Magnus Hagander <magnus@hagander.net>
2016-07-15 15:46:56 +0200

    Add lookups and mixin for handling selectable in admin classes

---
+++ postgresqleu/confreg/lookups.py
@@ -0,0 +1,33 @@
+from selectable.base import ModelLookup
+from selectable.registry import registry
+from selectable.decorators import staff_member_required
+
+from postgresqleu.confreg.models import ConferenceRegistration
+
+@staff_member_required
+class RegistrationLookup(ModelLookup):
+	model = ConferenceRegistration
+	search_fields = (
+		'attendee__username__icontains',
+		'firstname__icontains',
+		'lastname__icontains',
+		'email__icontains',
+	)
+
+	def get_query(self, request, term):
+		q = super(RegistrationLookup, self).get_query(request, term)
+		if request.GET.has_key('conference'):
+			return q.filter(conference_id=request.GET['conference'])
+		else:
+			# Don't return anything if parameter not present
+			return None
+
+	def get_item_value(self, item):
+		# Display for currently selected item
+		return u"%s (%s %s)" % (item.attendee.username, item.firstname, item.lastname)
+
+	def get_item_label(self, item):
+		# Display for choice listings
+		return u"%s (%s %s)" % (item.attendee.username, item.firstname, item.lastname)
+
+registry.register(RegistrationLookup)
