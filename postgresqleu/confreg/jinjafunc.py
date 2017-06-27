pgeu/pgeu-system

commit 4b62fb65c3eeadc0aafff44c9b1c7585ccbad84d

Use a variable for organization name and treasurer email address
This adds the variables:

ORG_NAME
TREASURER_EMAIL

to settings.py, to be adjusted by local_settings.py, and changes the
membership system to use those variables instead of hard-coded values.
This change allows other pgeu-website based systems to make use of the
general templates without having to override every template.

This also adjusts the jinja rendering to also use settings_context()
and adds these variables into the 'safe_attributes' for all of the
current models.

postgresqleu/confreg/jinjafunc.py
@@ -11,6 +11,8 @@ from itertools import groupby
 from datetime import datetime, date, time
 import dateutil.parser
 
+from postgresqleu.util.context_processors import settings_context
+
 import jinja2
 import jinja2.sandbox
 import markdown
@@ -226,5 +228,6 @@ def render_jinja_conference_response(request, conference, pagemagic, templatenam
 	})
 	if dictionary:
 		c.update(dictionary)
+	c.update(settings_context())
 
 	return HttpResponse(t.render(**c), content_type='text/html')

postgresqleu/util/context_processors.py
@@ -7,6 +7,9 @@ from django.conf import settings
 
 def settings_context(request):
 	return {
+		'org_name': settings.ORG_NAME,
+		'treasurer_email': settings.TREASURER_EMAIL,
+		'sitebase': settings.SITEBASE,
 		'currency_abbrev': settings.CURRENCY_ABBREV,
 		'currency_symbol': settings.CURRENCY_SYMBOL,
 		'is_debugging': settings.DEBUG,

postgresqleu/confreg/models.py
@@ -120,7 +120,7 @@ class Conference(models.Model):
 	_safe_attributes = ('active', 'askfood', 'askshareemail', 'asktshirt',
 						'callforpapersintro', 'callforpapersopen',
 						'conferencefeedbackopen', 'confurl', 'contactaddr',
-						'feedbackopen', 'skill_levels', 'urlname', )
+						'feedbackopen', 'org_name', 'skill_levels', 'treasurer_email', 'urlname', )
 
 	def __unicode__(self):
 		return self.conferencename
@@ -477,7 +477,7 @@ class Speaker(models.Model):
 	photofile = models.ImageField(upload_to=_get_upload_path, storage=SpeakerImageStorage(), blank=True, null=True, verbose_name="Photo")
 	lastmodified = models.DateTimeField(auto_now=True, null=False, blank=False)
 
-	_safe_attributes = ('id', 'name', 'fullname', 'twittername', 'company', 'abstract' ,'photofile', 'lastmodified')
+	_safe_attributes = ('id', 'name', 'fullname', 'twittername', 'company', 'abstract' ,'photofile', 'lastmodified', 'org_name', 'treasurer_email')
 	json_included_attributes = ['fullname', 'twittername', 'company', 'abstract', 'lastmodified']
 
 	@property

postgresqleu/confwiki/models.py
@@ -76,7 +76,7 @@ class WikipageHistory(models.Model):
 	publishedat = models.DateTimeField(null=False, blank=False)
 	contents = models.TextField(null=False, blank=False)
 
-	_safe_attributes = ('id', 'author', 'page', 'publishedat', 'contents', )
+	_safe_attributes = ('id', 'author', 'page', 'publishedat', 'contents', 'org_name', 'treasurer_email')
 
 	class Meta:
 		ordering = ('-publishedat',)
@@ -149,7 +149,7 @@ class AttendeeSignup(models.Model):
 	choice = models.CharField(max_length=100, null=False, blank=True)
 	saved = models.DateTimeField(null=False, blank=False, auto_now=True)
 
-	_safe_attributes = ('choice', 'saved', )
+	_safe_attributes = ('choice', 'org_name', 'saved', 'treasurer_email')
 
 	class Meta:
 		unique_together = (

postgresqleu/invoices/models.py
@@ -179,7 +179,7 @@ class VatRate(models.Model):
 									 validators=[MaxValueValidator(100), MinValueValidator(0)])
 	vataccount = models.ForeignKey(Account, null=False, blank=False)
 
-	_safe_attributes = ('vatpercent', 'shortstr', 'shortname', 'name')
+	_safe_attributes = ('vatpercent', 'shortstr', 'shortname', 'name', 'org_name', 'treasurer_email')
 
 	def __unicode__(self):
 		return u"{0} ({1}%)".format(self.name, self.vatpercent)

postgresqleu/membership/management/commands/membership_nightly.py
@@ -33,7 +33,7 @@ class Command(BaseCommand):
 						}))
 			send_simple_mail(settings.MEMBERSHIP_SENDER_EMAIL,
 							 m.user.email,
-							 "Your PostgreSQL Europe membership has expired",
+							 "Your " + settings.ORG_NAME + " membership has expired",
 							 txt)
 			self.stdout.write(u"Expired member {0} (paid until {1})".format(m, m.paiduntil))
 			# An expired member has no membersince and no paiduntil.
@@ -62,7 +62,7 @@ class Command(BaseCommand):
 						}))
 			send_simple_mail(settings.MEMBERSHIP_SENDER_EMAIL,
 							 m.user.email,
-							 "Your PostgreSQL Europe membership will expire soon",
+							 "Your " + settings.ORG_NAME + " membership will expire soon",
 							 txt)
 			self.stdout.write(u"Sent warning to member {0} (paid until {1}, last warned {2})".format(m, m.paiduntil, m.expiry_warning_sent))
 			m.expiry_warning_sent = datetime.now()

postgresqleu/membership/views.py
@@ -67,13 +67,13 @@ def home(request):
 					raise Exception("This should not happen - generating invoice when one already exists!")
 				manager = InvoiceManager()
 				processor = InvoiceProcessor.objects.get(processorname="membership processor")
-				invoicerows = [('PostgreSQL Europe - 2 years membership - %s' % request.user.email, 1, 10, None),]
+				invoicerows = [(settings.ORG_NAME + ' - 2 years membership - %s' % request.user.email, 1, 10, None),]
 				member.activeinvoice = manager.create_invoice(
 					request.user,
 					request.user.email,
 					request.user.first_name + ' ' + request.user.last_name,
 					'', # We don't have an address
-					'PostgreSQL Europe membership for %s'% request.user.email,
+					settings.ORG_NAME + ' membership for %s'% request.user.email,
 					datetime.now(),
 					datetime.now(),
 					invoicerows,

postgresqleu/settings.py
@@ -199,6 +199,12 @@ GLOBAL_LOGIN_PASSWORD=''
 # Set to true in local_settings.py to enable trustly integrations
 ENABLE_TRUSTLY=False
 
+# Organization name
+ORG_NAME="PostgreSQL Europe"
+
+# Treasurer email address
+TREASURER_EMAIL="treasurer@postgresql.eu"
+
 # If there is a local_settings.py, let it override our settings
 try:
 	from local_settings import *

commit 821298732d73beef7d2af163c642ee9b73b7a491

Fix unicode rendering problem in jinja templates
This was introduced in 4b62fb65 where settings_context was passed to all
jinja rendering, without verifying that the strings in it were either
ascii or unicode. As such it broke on everything referencing
CURRENCY_SYMBOL when it was set to '€'.

postgresqleu/confreg/jinjafunc.py
@@ -11,7 +11,7 @@ from itertools import groupby
 from datetime import datetime, date, time
 import dateutil.parser
 
-from postgresqleu.util.context_processors import settings_context
+from postgresqleu.util.context_processors import settings_context_unicode
 
 import jinja2
 import jinja2.sandbox
@@ -237,6 +237,6 @@ def render_jinja_conference_response(request, conference, pagemagic, templatenam
 
 	if dictionary:
 		c.update(dictionary)
-	c.update(settings_context())
+	c.update(settings_context_unicode())
 
 	return HttpResponse(t.render(**c), content_type='text/html')

postgresqleu/util/context_processors.py
@@ -14,3 +14,12 @@ def settings_context(request=None):
 		'currency_symbol': settings.CURRENCY_SYMBOL,
 		'is_debugging': settings.DEBUG,
 	}
+
+def settings_context_unicode(request=None):
+	# Same as settings_context, except convert all strings to unicode assuming
+	# utf-8.
+	c = settings_context(request)
+	for k,v in c.items():
+		if isinstance(v, str):
+			c[k] = v.decode('utf8')
+	return c