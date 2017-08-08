pgeu/pgeu-system

commit 463a3e1eb21e0a8af2893cfe52e51bf32029669c

Fix VAT in additional options preview
We got the VAT correct on the invoice, but the preview page would show
the non-VAT prices and not mention that VAT at all

postgresqleu/confreg/views.py
@@ -313,9 +313,7 @@ def reg_add_options(request, confname):
 		messages.warning(request, "Option searching mismatch, order canceled.")
 
 	# Check the count on each option (yes, this is inefficient, but who cares)
-	totalcost = 0
 	for o in options:
-		totalcost += o.cost
 		if o.maxcount > 0:
 			if o.conferenceregistration_set.count() >= o.maxcount:
 				messages.warning(request, "Option '{0}' is sold out.".format(o.name))
@@ -355,18 +353,41 @@ def reg_add_options(request, confname):
 
 	if new_regtype and new_regtype.cost >= reg.regtype.cost:
 		upsell_cost = new_regtype.cost - reg.regtype.cost
-		totalcost += upsell_cost
 	else:
 		upsell_cost = 0
+
+	# Build our invoice rows
+	invoicerows = []
+	if upsell_cost:
+		invoicerows.append(['Upgrade to {0}'.format(new_regtype.regtype), 1, upsell_cost, conference.vat_registrations])
+		if new_regtype.invoice_autocancel_hours:
+			autocancel_hours.append(new_regtype.invoice_autocancel_hours)
+
+	for o in options:
+		# Yes, we include €0 options for completeness.
+		invoicerows.append([o.name, 1, o.cost, conference.vat_registrations])
+		if o.invoice_autocancel_hours:
+			autocancel_hours.append(o.invoice_autocancel_hours)
+
+	# Add VAT information to invoice rows
+	for r in invoicerows:
+		# Calculate the with-vat information for this row
+		if r[3]:
+			r.append(r[2]*(100+r[3].vatpercent)/Decimal(100))
+		else:
+			r.append(r[2])
+
+	totalcost = sum([r[2] for r in invoicerows])
+	totalwithvat = sum([r[4] for r in invoicerows])
 
 	if not request.POST.get('confirm', None) == 'yes':
 		# Generate a preview
 		return render_conference_response(request, conference, 'reg', 'confreg/confirm_addons.html', {
 			'reg': reg,
 			'options': options,
-			'newreg': new_regtype,
-			'upsell_cost': upsell_cost,
+			'invoicerows': invoicerows,
 			'totalcost': totalcost,
+			'totalwithvat': totalwithvat,
 		})
 	else:
 		if totalcost == 0:
@@ -380,20 +401,7 @@ def reg_add_options(request, confname):
 			messages.info(request, 'Additional options added to registration')
 			return HttpResponseRedirect('../')
 
-		# Build our invoice rows
-		invoicerows = []
 		autocancel_hours = [conference.invoice_autocancel_hours, ]
-
-		if upsell_cost:
-			invoicerows.append(('Upgrade to {0}'.format(new_regtype.regtype), 1, upsell_cost, conference.vat_registrations))
-			if new_regtype.invoice_autocancel_hours:
-				autocancel_hours.append(new_regtype.invoice_autocancel_hours)
-
-		for o in options:
-			# Yes, we include €0 options for completeness.
-			invoicerows.append((o.name, 1, o.cost, conference.vat_registrations))
-			if o.invoice_autocancel_hours:
-				autocancel_hours.append(o.invoice_autocancel_hours)
 
 		# Create a pending addon order, and generate an invoice
 		order = PendingAdditionalOrder(reg=reg,

template.jinja/confreg/confirm_addons.html
@@ -36,6 +36,9 @@ registration. If this is in any way incorrect, please contact
 </p>
 
 <form method="post" action="." class="pgeuregformconfirm">{{ csrf_input }}
+{%for o in options%}
+<input type="hidden" name="ao_{{o.id}}" value="1">
+{%endfor%}
 <fieldset id="fldset_recipient">
 <legend>Invoice recipient</legend>
 <div>
@@ -49,21 +52,31 @@ registration. If this is in any way incorrect, please contact
 <fieldset id="fldset_contents">
 <legend>Invoice contents</legend>
 <table border="0" cellspacing="0" cellpadding="1" class="invoiceconfirmtbl">
-{%if upsell_cost%}
 <tr valign="top">
-  <td class="desc">Upgrade to '{{newreg.regtype}}'</td>
-  <td class="cost">{{upsell_cost|currency_format}}</td>
-</tr>
+  <td class="desc"></td>
+  <td class="cost">Amount</td>
+{%if totalwithvat != totalcost%}
+  <td class="cost">VAT</td>
+  <td class="cost">Total</td>
 {%endif%}
-{%for o in options%}
+</tr>
+{%for r in invoicerows%}
 <tr valign="top">
-  <td class="desc">{{o.name}} <input type="hidden" name="ao_{{o.id}}" value="1"></td>
-  <td class="cost">{{o.cost|currency_format}}</td>
+  <td class="desc">{{r.0|leadingnbsp}}</td>
+  <td class="cost">{{r.2|currency_format}}</td>
+{%if totalwithvat != totalcost %}
+  <td class="cost">{{r.3.vatpercent}}%</td>
+  <td class="cost">{{r.4|currency_format}}</td>
+{%endif%}
 </tr>
 {%endfor%}
 <tr valign="top" class="totalrow">
   <td class="desc"></td>
   <td class="cost">{{totalcost|currency_format}}</td>
+{%if totalwithvat != totalcost %}
+  <td class="cost"></td>
+  <td class="cost">{{totalwithvat|currency_format}}</td>
+{%endif%}
 </tr>
 </table>
 </fieldset>

commit 5e0cec47551500c07df2aa8664e2d40da8b18356

Fix variable initialization for invoice auto canceling
This was broken in 463a3e1e which fixed VAT previews for additional
options. In doing so it needed access to the autocancel information at
an earlier stage, but the initialization of the variable was not
properly moved.

postgresqleu/confreg/views.py
@@ -358,6 +358,7 @@ def reg_add_options(request, confname):
 
 	# Build our invoice rows
 	invoicerows = []
+	autocancel_hours = [conference.invoice_autocancel_hours, ]
 	if upsell_cost:
 		invoicerows.append(['Upgrade to {0}'.format(new_regtype.regtype), 1, upsell_cost, conference.vat_registrations])
 		if new_regtype.invoice_autocancel_hours:
@@ -400,8 +401,6 @@ def reg_add_options(request, confname):
 			reg.save()
 			messages.info(request, 'Additional options added to registration')
 			return HttpResponseRedirect('../')
-
-		autocancel_hours = [conference.invoice_autocancel_hours, ]
 
 		# Create a pending addon order, and generate an invoice
 		order = PendingAdditionalOrder(reg=reg,