c66e0f734623695aa458ee83d09938e1b6887b3e
Magnus Hagander <magnus@hagander.net>
2017-09-09 16:10:13 +0200

    Add missing migration
    This is a no-op migration since the list of available values is static,
    but django inists on there being one.

---
+++ postgresqleu/confreg/migrations/0007_new_specialtype.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('confreg', '0006_jinjadir'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='registrationtype',
+            name='specialtype',
+            field=models.CharField(blank=True, max_length=5, null=True, choices=[(b'man', b'Manually confirmed'), (b'spk', b'Confirmed speaker'), (b'spkr', b'Confirmed or reserve speaker'), (b'staff', b'Confirmed staff')]),
+        ),
+    ]
