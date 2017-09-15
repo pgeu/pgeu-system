pgeu/pgeu-system

commit bc8e3fd31c428c9b9413caef34081cc73195e10b

Introduce send_template_mail
This introduces the function of the same name from the pgweb code base
(though it isn't identical, to be clear).  This function is to be used
when rendering a text file into an email.  There is a helper function
template_to_string() which is also added and can be used if a string
result is needed.

The primary different here is that the variables set in
util/context_processor:settings_context are included in the hash
automatically, so those settings do not need to be explicitly set by
the callers and the templates can expect them to be available.

Using that, this change also includes changes to move away from
hard-coded values like 'PostgreSQL Europe' in favor of using the
variables set in settings.py/local_settings.py in the text templates.
This will allow others to re-use the exisitng templates and not have
to develop and maintain their own going forward.

postgresqleu/confreg/views.py
@@ -42,7 +42,7 @@ from postgresqleu.invoices.models import Invoice, InvoicePaymentMethod, InvoiceR
 from postgresqleu.confwiki.models import Wikipage
 from postgresqleu.invoices.util import InvoiceManager, InvoicePresentationWrapper
 from postgresqleu.invoices.models import InvoiceProcessor
-from postgresqleu.mailqueue.util import send_mail, send_simple_mail
+from postgresqleu.mailqueue.util import send_mail, send_simple_mail, send_template_mail, template_to_string
 from postgresqleu.util.jsonutil import JsonSerializer
 
 from decimal import Decimal
@@ -962,19 +962,18 @@ def callforpapers_confirm(request, confname, sessionid):
 			session.status = 1 # Now approved!
 			session.save()
 			# We can generate the email for this right away, so let's do that
-			template = get_template('confreg/mail/session_notify.txt')
 			for spk in session.speaker.all():
-				send_simple_mail(conference.contactaddr,
-								 spk.user.email,
-								 "Your session '%s' submitted to %s" % (session.title, conference),
-								 template.render(Context({
+				send_template_mail(conference.contactaddr,
+								   spk.user.email,
+								   "Your session '%s' submitted to %s" % (session.title, conference),
+								   'confreg/mail/session_notify.txt',
+								   {
 									 'conference': conference,
 									 'session': session,
-									 'SITEBASE': settings.SITEBASE,
-									 })),
-								 sendername = conference.conferencename,
-								 receivername = spk.fullname,
-							 )
+								   },
+								   sendername = conference.conferencename,
+								   receivername = spk.fullname,
+							   )
 			session.lastnotifiedstatus = 1 # Now also approved
 			session.lastnotifiedtime = datetime.now()
 			session.save()
@@ -1377,11 +1376,10 @@ def viewvouchers(request, batchid):
 
 	vouchers = batch.prepaidvoucher_set.all()
 
-	vouchermailtext = get_template('confreg/mail/prepaid_vouchers.txt').render(Context({
+	vouchermailtext = template_to_string('confreg/mail/prepaid_vouchers.txt'),{
 		'batch': batch,
 		'vouchers': vouchers,
-		'SITEBASE': settings.SITEBASE,
-		}))
+		}
 
 	return render_to_response('confreg/prepaid_create_list.html', {
 			'batch': batch,
@@ -1397,18 +1395,17 @@ def emailvouchers(request, batchid):
 	batch = PrepaidBatch.objects.get(pk=batchid)
 	vouchers = batch.prepaidvoucher_set.all()
 
-	vouchermailtext = get_template('confreg/mail/prepaid_vouchers.txt').render(Context({
-		'batch': batch,
-		'vouchers': vouchers,
-		'SITEBASE': settings.SITEBASE,
-	}))
-	send_simple_mail(batch.conference.contactaddr,
-					  batch.buyer.email,
-					  "Attendee vouchers for %s" % batch.conference,
-					  vouchermailtext,
-					 sendername=batch.conference.conferencename,
-					 receivername=u"{0} {1}".format(batch.buyer.first_name, batch.buyer.last_name),
-					  )
+	send_template_mail(batch.conference.contactaddr,
+					   batch.buyer.email,
+					   "Attendee vouchers for %s" % batch.conference,
+					   'confreg/mail/prepaid_vouchers.txt',
+					   {
+						   'batch': batch,
+						   'vouchers': vouchers,
+					   },
+					   sendername=batch.conference.conferencename,
+					   receivername=u"{0} {1}".format(batch.buyer.first_name, batch.buyer.last_name),
+				   )
 	return HttpResponse('OK')
 
 @login_required
@@ -1510,9 +1507,6 @@ def bulkpay(request, confname):
 					bp.numregs = len(allregs)
 					bp.save() # Save so we get a primary key
 
-					# Get a template for notifying the attendees by mail
-					template = get_template('confreg/mail/bulkpay_added.txt')
-
 					# Now assign this bulk record to all our registrations
 					for r in allregs:
 						r.bulkpayment = bp
@@ -1524,17 +1518,18 @@ def bulkpay(request, confname):
 
 						# Also notify these registrants that they have been
 						# added to the bulk payment.
-						send_simple_mail(conference.contactaddr,
-										 r.email,
-										 "Your registration for {0} added to bulk payment".format(conference.conferencename),
-										 template.render(Context({
-											 'conference': conference,
-											 'reg': r,
-											 'bulk': bp,
-										 })),
-										 sendername = conference.conferencename,
-										 receivername = r.fullname,
-									 )
+						send_template_mail(conference.contactaddr,
+										   r.email,
+										   "Your registration for {0} added to bulk payment".format(conference.conferencename),
+										   'confreg/mail/bulkpay_added.txt',
+										   {
+											   'conference': conference,
+											   'reg': r,
+											   'bulk': bp,
+										   },
+										   sendername = conference.conferencename,
+										   receivername = r.fullname,
+									   )
 
 					# Finally, create an invoice for it
 					manager = InvoiceManager()
@@ -2071,7 +2066,7 @@ def admin_waitlist(request, urlname):
 				raise Exception("Database lookup mismatch")
 			if len(regs) < 1:
 				raise Exception("Somehow got through with zero!")
-			template = get_template('confreg/mail/waitlist_offer.txt')
+
 			for r in regs:
 				wl = r.registrationwaitlistentry
 				if wl.offeredon:
@@ -2081,18 +2076,18 @@ def admin_waitlist(request, urlname):
 				wl.save()
 				RegistrationWaitlistHistory(waitlist=wl,
 											text="Made offer valid for {0} hours by {1}".format(form.cleaned_data['hours'], request.user.username)).save()
-				send_simple_mail(conference.contactaddr,
-								 r.email,
-								 "Your waitlisted registration for {0}".format(conference.conferencename),
-								 template.render(Context({
-									 'conference': conference,
-									 'reg': r,
-									 'offerexpires': wl.offerexpires,
-									 'SITEBASE': settings.SITEBASE,
-									 })),
-								 sendername = conference.conferencename,
-								 receivername = u"{0} {1}".format(r.firstname, r.lastname),
-								 )
+				send_template_mail(conference.contactaddr,
+								   r.email,
+								   "Your waitlisted registration for {0}".format(conference.conferencename),
+								   'confreg/mail/waitlist_offer.txt',
+								   {
+									   'conference': conference,
+									   'reg': r,
+									   'offerexpires': wl.offerexpires,
+								   },
+								   sendername = conference.conferencename,
+								   receivername = u"{0} {1}".format(r.firstname, r.lastname),
+							   )
 				messages.info(request, "Sent offer to {0}".format(r.email))
 			return HttpResponseRedirect(".")
 	else:
@@ -2181,20 +2176,19 @@ def session_notify_queue(request, urlname):
 	if request.method == 'POST' and request.POST.has_key('confirm_sending') and request.POST['confirm_sending'] == '1':
 		# Ok, it would appear we should actually send them...
 		num = 0
-		template = get_template('confreg/mail/session_notify.txt')
 		for s in notifysessions:
 			for spk in s.speaker.all():
-				send_simple_mail(conference.contactaddr,
-								 spk.user.email,
-								 "Your session '%s' submitted to %s" % (s.title, conference),
-								 template.render(Context({
-									 'conference': conference,
-									 'session': s,
-									 'SITEBASE': settings.SITEBASE,
-									 })),
-								 sendername=conference.conferencename,
-								 receivername=spk.fullname,
-							 )
+				send_template_mail(conference.contactaddr,
+								   spk.user.email,
+								   "Your session '%s' submitted to %s" % (s.title, conference),
+								   'confreg/mail/session_notify.txt',
+								   {
+									   'conference': conference,
+									   'session': s,
+								   },
+								   sendername=conference.conferencename,
+								   receivername=spk.fullname,
+							   )
 				num += 1
 			s.lastnotifiedstatus = s.status
 			s.lastnotifiedtime = datetime.now()

postgresqleu/confreg/invoicehandler.py
@@ -1,8 +1,6 @@
 from django.conf import settings
-from django.template import Context
-from django.template.loader import get_template
 
-from postgresqleu.mailqueue.util import send_simple_mail
+from postgresqleu.mailqueue.util import send_template_mail
 from models import ConferenceRegistration, BulkPayment, PendingAdditionalOrder
 from models import RegistrationWaitlistHistory
 from util import notify_reg_confirmed, expire_additional_options
@@ -160,23 +158,22 @@ class BulkInvoiceProcessor(object):
 		# Unlink this bulk payment from all registrations. This will
 		# automatically unlock the registrations. Also notify the
 		# attendees that this happened.
-		template = get_template('confreg/mail/bulkpay_canceled.txt')
-
 		for r in bp.conferenceregistration_set.all():
 			r.bulkpayment = None
 			r.save()
 
-			send_simple_mail(bp.conference.contactaddr,
-							 r.email,
-							 "Your registration for {0} bulk payment canceled".format(bp.conference.conferencename),
-							 template.render(Context({
-								 'conference': bp.conference,
-								 'reg': r,
-								 'bulk': bp,
-							 })),
-							 sendername = bp.conference.conferencename,
-							 receivername = r.fullname,
-						 )
+			send_template_mail(bp.conference.contactaddr,
+							   r.email,
+							   "Your registration for {0} bulk payment canceled".format(bp.conference.conferencename),
+							   'confreg/mail/bulkpay_canceled.txt',
+							   {
+								   'conference': bp.conference,
+								   'reg': r,
+								   'bulk': bp,
+							   },
+							   sendername = bp.conference.conferencename,
+							   receivername = r.fullname,
+						   )
 
 			# If this registration holds any additional options that are about to expire, release
 			# them for others to use at this point. (This will send an additional email to the
@@ -211,25 +208,24 @@ class BulkInvoiceProcessor(object):
 		# Unlink this bulk payment from all registrations. This will
 		# automatically unlock the registrations. Also inform the
 		# attendee that this happened.
-		template = get_template('confreg/mail/bulkpay_refunded.txt')
-
 		for r in bp.conferenceregistration_set.all():
 			r.bulkpayment = None
 			r.payconfirmedat = None
 			r.payconfirmedby = None
 			r.save()
 
-			send_simple_mail(bp.conference.contactaddr,
-							 r.email,
-							 "Your registration for {0} bulk payment refunded".format(bp.conference.conferencename),
-							 template.render(Context({
-								 'conference': bp.conference,
-								 'reg': r,
-								 'bulk': bp,
-							 })),
-							 sendername = bp.conference.conferencename,
-							 receivername = r.fullname,
-						 )
+			send_template_mail(bp.conference.contactaddr,
+							   r.email,
+							   "Your registration for {0} bulk payment refunded".format(bp.conference.conferencename),
+							   'confreg/mail/bulkpay_refunded.txt',
+							   {
+								   'conference': bp.conference,
+								   'reg': r,
+								   'bulk': bp,
+							   },
+							   sendername = bp.conference.conferencename,
+							   receivername = r.fullname,
+						   )
 
 		# Now actually *remove* the bulk payment record completely,
 		# since it no longer contains anything interesting.

postgresqleu/confreg/management/commands/confreg_expire_waitlist.py
@@ -12,9 +12,8 @@ from django.conf import settings
 from datetime import datetime
 
 from django.template import Context
-from django.template.loader import get_template
 
-from postgresqleu.mailqueue.util import send_simple_mail
+from postgresqleu.mailqueue.util import send_simple_mail, send_template_mail
 
 from postgresqleu.confreg.models import RegistrationWaitlistEntry, RegistrationWaitlistHistory
 
@@ -27,8 +26,6 @@ class Command(BaseCommand):
 		# system, as the expiry time of the invoice is set synchronized. In this
 		# run, we only care about offers that have not been picked up at all.
 		wlentries = RegistrationWaitlistEntry.objects.filter(registration__payconfirmedat__isnull=True, registration__invoice__isnull=True, offerexpires__lt=datetime.now())
-
-		template = get_template('confreg/mail/waitlist_expired.txt')
 
 		for w in wlentries:
 			reg = w.registration
@@ -45,18 +42,18 @@ class Command(BaseCommand):
 								 sendername=reg.conference.conferencename)
 
 			# Also send an email to the user
-			send_simple_mail(reg.conference.contactaddr,
-							 reg.email,
-							 'Your waitlist offer for {0}'.format(reg.conference.conferencename),
-							 template.render(Context({
-								 'conference': reg.conference,
-								 'reg': reg,
-								 'offerexpires': w.offerexpires,
-								 'SITEBASE': settings.SITEBASE,
-								 })),
-							 sendername = reg.conference.conferencename,
-							 receivername = u"{0} {1}".format(reg.firstname, reg.lastname),
-							 )
+			send_template_mail(reg.conference.contactaddr,
+							   reg.email,
+							   'Your waitlist offer for {0}'.format(reg.conference.conferencename),
+							   'confreg/mail/waitlist_expired.txt',
+							   {
+								   'conference': reg.conference,
+								   'reg': reg,
+								   'offerexpires': w.offerexpires,
+							   },
+							   sendername = reg.conference.conferencename,
+							   receivername = u"{0} {1}".format(reg.firstname, reg.lastname),
+						   )
 
 			# Now actually expire the offer
 			w.offeredon = None

postgresqleu/confreg/management/commands/confreg_send_reminders.py
@@ -12,10 +12,7 @@ from django.conf import settings
 from StringIO import StringIO
 from datetime import datetime, timedelta
 
-from django.template import Context
-from django.template.loader import get_template
-
-from postgresqleu.mailqueue.util import send_simple_mail
+from postgresqleu.mailqueue.util import send_simple_mail, send_template_mail
 
 from postgresqleu.confreg.models import Conference, Speaker, ConferenceSession
 from postgresqleu.confreg.models import ConferenceRegistration
@@ -76,7 +73,6 @@ class Command(BaseCommand):
 										  conferencesession__lastnotifiedtime__lt=datetime.now()-timedelta(days=7)).distinct()
 		if speakers:
 			whatstr.write("Found {0} unconfirmed talks:\n".format(len(speakers)))
-			template = get_template('confreg/mail/speaker_remind_confirm.txt')
 
 			for speaker in speakers:
 				sessions = speaker.conferencesession_set.filter(conference=conference, status=3)
@@ -84,17 +80,17 @@ class Command(BaseCommand):
 					s.lastnotifiedtime = datetime.now()
 					s.save()
 
-				send_simple_mail(conference.contactaddr,
-								 speaker.user.email,
-								 "Your submissions to {0}".format(conference),
-								 template.render(Context({
+				send_template_mail(conference.contactaddr,
+								   speaker.user.email,
+								   "Your submissions to {0}".format(conference),
+								   'confreg/mail/speaker_remind_confirm.txt',
+								   {
 									 'conference': conference,
 									 'sessions': sessions,
-									 'SITEBASE': settings.SITEBASE,
-								 })),
-								 sendername = conference.conferencename,
-								 receivername = speaker.fullname,
-							 )
+								   },
+								   sendername = conference.conferencename,
+								   receivername = speaker.fullname,
+							   )
 
 				whatstr.write(u"Reminded speaker {0} to confirm {1} talks\n".format(speaker, len(sessions)))
 
@@ -107,7 +103,6 @@ class Command(BaseCommand):
 											[conference.id, datetime.now()-timedelta(days=7), conference.id]))
 		if speakers:
 			whatstr.write("Found {0} unregistered speakers:\n".format(len(speakers)))
-			template = get_template('confreg/mail/speaker_remind_register.txt')
 			for speaker in speakers:
 				# Update the last notified date on all sessions that are in
 				# status approved, to make sure we don't send a second
@@ -119,16 +114,16 @@ class Command(BaseCommand):
 					lastnotifiedtime=datetime.now()
 				)
 
-				send_simple_mail(conference.contactaddr,
-								 speaker.user.email,
-								 "Your registration to {0}".format(conference),
-								 template.render(Context({
+				send_template_mail(conference.contactaddr,
+								   speaker.user.email,
+								   "Your registration to {0}".format(conference),
+								   'confreg/mail/speaker_remind_register.txt',
+								   {
 									 'conference': conference,
-									 'SITEBASE': settings.SITEBASE,
-								 })),
-								 sendername = conference.conferencename,
-								 receivername = speaker.fullname,
-							 )
+								   },
+								   sendername = conference.conferencename,
+								   receivername = speaker.fullname,
+							   )
 
 				whatstr.write(u"Reminded speaker {0} to register\n".format(speaker))
 
@@ -152,19 +147,18 @@ class Command(BaseCommand):
 
 		if regs:
 			whatstr.write("Found {0} unconfirmed registrations that are stalled:\n".format(len(regs)))
-			template = get_template('confreg/mail/attendee_stalled_registration.txt')
 			for reg in regs:
-				send_simple_mail(conference.contactaddr,
-								 reg.email,
-								 "Your registration to {0}".format(conference),
-								 template.render(Context({
-									 'conference': conference,
-									 'reg': reg,
-									 'SITEBASE': settings.SITEBASE,
-								 })),
-								 sendername = conference.conferencename,
-								 receivername = reg.fullname,
-							 )
+				send_template_mail(conference.contactaddr,
+								   reg.email,
+								   "Your registration to {0}".format(conference),
+								   'confreg/mail/attendee_stalled_registration.txt',
+								   {
+									   'conference': conference,
+									   'reg': reg,
+								   },
+								   sendername = conference.conferencename,
+								   receivername = reg.fullname,
+							   )
 				reg.lastmodified = datetime.now()
 				reg.save()
 
@@ -177,23 +171,21 @@ class Command(BaseCommand):
 		# 3 days (this will also make the reminder show up every 3 days, and not every day, since we touch
 		# the lastmodified timestemp when a reminder is sent).
 
-		template = get_template('confreg/mail/speaker_empty_submission.txt')
-
 		for sess in conference.conferencesession_set.filter(abstract='',
 															status=0,
 															lastmodified__lt=datetime.now()-timedelta(days=3)):
 			for spk in sess.speaker.all():
-				send_simple_mail(conference.contactaddr,
-								 spk.email,
-								 "Your submission to {0}".format(conference),
-								 template.render(Context({
-									 'conference': conference,
-									 'session': sess,
-									 'SITEBASE': settings.SITEBASE,
-								 })),
-								 sendername = conference.conferencename,
-								 receivername = spk.name,
-							 )
+				send_template_mail(conference.contactaddr,
+								   spk.email,
+								   "Your submission to {0}".format(conference),
+								   'confreg/mail/speaker_empty_submission.txt',
+								   {
+									   'conference': conference,
+									   'session': sess,
+								   },
+								   sendername = conference.conferencename,
+								   receivername = spk.name,
+							   )
 				whatstr.write(u"Reminded speaker {0} that they have made an empty submission\n".format(spk.name))
 			sess.lastmodified = datetime.now()
 			sess.save()
@@ -208,19 +200,18 @@ class Command(BaseCommand):
 										  abstract='',
 									  ).distinct()
 
-		template = get_template('confreg/mail/speaker_empty_profile.txt')
 		for spk in speakers:
-			send_simple_mail(conference.contactaddr,
-							 spk.email,
-							 "Your submission to {0}".format(conference),
-							 template.render(Context({
-								 'conference': conference,
-								 'speaker': spk,
-								 'SITEBASE': settings.SITEBASE,
-							 })),
-							 sendername = conference.conferencename,
-							 receivername = spk.name,
-			)
+			send_template_mail(conference.contactaddr,
+							   spk.email,
+							   "Your submission to {0}".format(conference),
+							   'confreg/mail/speaker_empty_profile.txt',
+							   {
+								   'conference': conference,
+								   'speaker': spk,
+							   },
+							   sendername = conference.conferencename,
+							   receivername = spk.name,
+						   )
 			spk.lastmodified = datetime.now()
 			spk.save()
 			whatstr.write(u"Reminded speaker {0} that their profile is empty\n".format(spk.name))

postgresqleu/confreg/util.py
@@ -1,11 +1,10 @@
 from django.conf import settings
 from django.template import Context
-from django.template.loader import get_template
 
 from datetime import datetime, date, timedelta
 from decimal import Decimal
 
-from postgresqleu.mailqueue.util import send_simple_mail
+from postgresqleu.mailqueue.util import send_simple_mail, send_template_mail
 
 from models import PrepaidVoucher, DiscountCode, RegistrationWaitlistHistory
 
@@ -149,20 +148,19 @@ def expire_additional_options(reg):
 		# template. (It's a bit inefficient to re-parse the template every time, but
 		# we don't expire these things very often, so we don't care)
 
-		template = get_template('confreg/mail/additionaloption_expired.txt')
-
-		send_simple_mail(reg.conference.contactaddr,
-						 reg.email,
-						 'Your pending registration for {0}'.format(reg.conference.conferencename),
-						 template.render(Context({
+		send_template_mail(reg.conference.contactaddr,
+						   reg.email,
+						   'Your pending registration for {0}'.format(reg.conference.conferencename),
+						   'confreg/mail/additionaloption_expired.txt',
+						   {
 							 'conference': reg.conference,
 							 'reg': reg,
 							 'options': expireset,
 							 'optionscount': len(expireset),
-							 'SITEBASE': settings.SITEBASE,
-						 })),
-						 sendername = reg.conference.conferencename,
-						 receivername = u"{0} {1}".format(reg.firstname, reg.lastname))
+						   },
+						   sendername = reg.conference.conferencename,
+						   receivername = u"{0} {1}".format(reg.firstname, reg.lastname)
+					   )
 
 		for ao in expireset:
 			# Notify caller that this one is being expired

postgresqleu/confsponsor/benefitclasses/entryvouchers.py
@@ -1,7 +1,6 @@
 from django import forms
 from django.core.validators import MaxValueValidator
 from django.template import Context
-from django.template.loader import get_template
 
 import base64
 import os
@@ -10,7 +9,7 @@ import cStringIO as StringIO
 
 from base import BaseBenefit
 
-from postgresqleu.mailqueue.util import send_simple_mail
+from postgresqleu.mailqueue.util import send_template_mail
 
 from postgresqleu.confreg.models import RegistrationType, PrepaidBatch, PrepaidVoucher
 
@@ -76,13 +75,15 @@ class EntryVouchers(BaseBenefit):
 				vouchers.append(v)
 
 			# Send an email about the new vouchers
-			send_simple_mail(self.level.conference.sponsoraddr,
-							 request.user.email,
-							 "Entry vouchers for {0}".format(self.level.conference),
-							 get_template('confreg/mail/prepaid_vouchers.txt').render(Context({
+			send_template_mail(self.level.conference.sponsoraddr,
+							   request.user.email,
+							   "Entry vouchers for {0}".format(self.level.conference),
+							   'confreg/mail/prepaid_vouchers.txt',
+							   {
 								 'batch': batch,
 								 'vouchers': vouchers,
-								 })))
+							   }
+						   )
 
 			# Finally, finish the claim
 			claim.claimdata = batch.id

postgresqleu/confsponsor/invoicehandler.py
@@ -1,12 +1,11 @@
 from django.conf import settings
 from django.template import Context
-from django.template.loader import get_template
 
 from datetime import datetime, timedelta, date
 import base64
 import os
 
-from postgresqleu.mailqueue.util import send_simple_mail
+from postgresqleu.mailqueue.util import send_simple_mail, send_template_mail
 from postgresqleu.invoices.util import InvoiceManager
 
 from models import Sponsor, PurchasedVoucher
@@ -22,18 +21,17 @@ def confirm_sponsor(sponsor, who):
 	sponsor.confirmedby = who
 	sponsor.save()
 
-	msgtxt = get_template('confsponsor/mail/sponsor_confirmed.txt').render(Context({
-		'sponsor': sponsor,
-		'conference': sponsor.conference,
-		'SITEBASE': settings.SITEBASE,
-	}))
 	for manager in sponsor.managers.all():
-		send_simple_mail(sponsor.conference.sponsoraddr,
-						 manager.email,
-						 u"[{0}] Sponsorship confirmed".format(sponsor.conference),
-						 msgtxt,
-						 sendername=sponsor.conference.conferencename,
-						 receivername=u'{0} {1}'.format(manager.first_name, manager.last_name))
+		send_template_mail(sponsor.conference.sponsoraddr,
+						   manager.email,
+						   u"[{0}] Sponsorship confirmed".format(sponsor.conference),
+						   'confsponsor/mail/sponsor_confirmed.txt',
+						   {
+							   'sponsor': sponsor,
+							   'conference': sponsor.conference,
+						   },
+						   sendername=sponsor.conference.conferencename,
+						   receivername=u'{0} {1}'.format(manager.first_name, manager.last_name))
 
 
 class InvoiceProcessor(object):

postgresqleu/confsponsor/management/commands/sponsor_generate_discount_invoices.py
@@ -9,12 +9,10 @@ from django.conf import settings
 from datetime import date, datetime, timedelta
 
 from django.db.models import Q, F, Count
-from django.template import Context
-from django.template.loader import get_template
 
 from postgresqleu.confreg.models import DiscountCode
 from postgresqleu.confsponsor.models import Sponsor # Required for text based resolving in DiscountCode
-from postgresqleu.mailqueue.util import send_simple_mail
+from postgresqleu.mailqueue.util import send_simple_mail, send_template_mail
 from postgresqleu.invoices.util import InvoiceManager, InvoiceWrapper
 
 class Command(BaseCommand):
@@ -41,20 +39,19 @@ class Command(BaseCommand):
 								 code.conference.sponsoraddr,
 								 u"[{0}] Discount code expired".format(code.conference),
 								 u"Discount code {0} has expired without any uses.".format(code.code))
-				msg = get_template('confsponsor/mail/discount_expired.txt').render(Context({
-					'code': code,
-					'sponsor': code.sponsor,
-					'conference': code.conference,
-					'SITEBASE': settings.SITEBASE,
-				}))
-				for manager in code.sponsor.managers.all():
-					send_simple_mail(code.conference.sponsoraddr,
-									 manager.email,
-									 u"[{0}] Discount code {1} expired".format(code.conference, code.code),
-									 msg,
-									 sendername=code.conference.conferencename,
 
-									 receivername=u'{0} {1}'.format(manager.first_name, manager.last_name))
+				for manager in code.sponsor.managers.all():
+					send_template_mail(code.conference.sponsoraddr,
+									   manager.email,
+									   u"[{0}] Discount code {1} expired".format(code.conference, code.code),
+									   'confsponsor/mail/discount_expired.txt',
+									   {
+										   'code': code,
+										   'sponsor': code.sponsor,
+										   'conference': code.conference,
+									   },
+									   sendername=code.conference.conferencename,
+									   receivername=u'{0} {1}'.format(manager.first_name, manager.last_name))
 			else:
 				# At least one use, so we generate the invoice
 				invoicerows = []
@@ -102,19 +99,20 @@ class Command(BaseCommand):
 									 len(invoicerows),
 									 code.invoice.total_amount,
 								 ))
-				msg = get_template('confsponsor/mail/discount_invoiced.txt').render(Context({
-					'code': code,
-					'conference': code.conference,
-					'sponsor': code.sponsor,
-					'invoice': code.invoice,
-					'curr': settings.CURRENCY_ABBREV,
-					'expired_time': code.validuntil < date.today(),
-					'SITEBASE': settings.SITEBASE,
-				}))
+
 				for manager in code.sponsor.managers.all():
-					send_simple_mail(code.conference.sponsoraddr,
-									 manager.email,
-									 u"[{0}] Discount code {1} has been invoiced".format(code.conference, code.code),
-									 msg,
-									 sendername=code.conference.conferencename,
-									 receivername=u'{0} {1}'.format(manager.first_name, manager.last_name))
+					send_template_mail(code.conference.sponsoraddr,
+									   manager.email,
+									   u"[{0}] Discount code {1} has been invoiced".format(code.conference, code.code),
+									   'confsponsor/mail/discount_invoiced.txt',
+									   {
+										   'code': code,
+										   'conference': code.conference,
+										   'sponsor': code.sponsor,
+										   'invoice': code.invoice,
+										   'curr': settings.CURRENCY_ABBREV,
+										   'expired_time': code.validuntil < date.today(),
+									   },
+									   sendername=code.conference.conferencename,
+									   receivername=u'{0} {1}'.format(manager.first_name, manager.last_name)
+								   )

commit 9a38883d1de36b77f621e0d2445f6bbaa20b4727

Fix misplaced parenthesis
Bug introduced in bc8e3fd3 that introduced send_template_mail. With this
misplaced parnethesis the mail still rendered -- but got the
placeholders in the output and was returned as an array instead of a
string (which still delivers in the browser though).

postgresqleu/confreg/views.py
@@ -1387,10 +1387,10 @@ def viewvouchers(request, batchid):
 
 	vouchers = batch.prepaidvoucher_set.all()
 
-	vouchermailtext = template_to_string('confreg/mail/prepaid_vouchers.txt'),{
+	vouchermailtext = template_to_string('confreg/mail/prepaid_vouchers.txt',{
 		'batch': batch,
 		'vouchers': vouchers,
-		}
+		})
 
 	return render_to_response('confreg/prepaid_create_list.html', {
 			'batch': batch,