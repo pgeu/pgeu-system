11108faa241ba787156a1105174da65abf79e56c
Magnus Hagander <magnus@hagander.net>
2017-10-25 13:47:10 +0200

    Fix merge error

--- postgresqleu/confreg/migrations/0009_confslides.py
+++ postgresqleu/confreg/migrations/0009_confslides.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('confreg', '0008_separate_registrator'),
+        ('confreg', '0008_volunteers'),
     ]
 
     operations = [
