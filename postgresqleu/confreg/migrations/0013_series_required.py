3b4f0ed12731a2335b8fb5fe4205f4f27e18e441
Magnus Hagander <magnus@hagander.net>
2017-12-09 15:52:40 +0100

    Make conference series a mandatory field
    Since we use this to drive opt-outs, we need it to actually exist for
    all conferences.
    
    Making this a separate migration to make it possible to go in and
    manually add series for all conferences when this migration fails :)

---
+++ postgresqleu/confreg/migrations/0013_series_required.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('confreg', '0012_mandatory_tokens'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='conference',
+            name='series',
+            field=models.ForeignKey(to='confreg.ConferenceSeries'),
+        ),
+    ]
