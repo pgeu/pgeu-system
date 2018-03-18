3f8797feff1ba617f8ed6ad42dca8d48600aee84
Magnus Hagander <magnus@hagander.net>
2018-03-18 14:38:51 +0100

    Add check constraint to enforce payment fields
    We cannot have both an invoice_id and a bulkpayment_id on a registration
    (because it can really only be paid once). Enforce this with a CHECK
    constraint.

---
+++ postgresqleu/confreg/migrations/0018_constraint_reg_payments.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('confreg', '0017_payconfirmedat_timestamp'),
+    ]
+
+    operations = [
+		migrations.RunSQL("ALTER TABLE confreg_conferenceregistration ADD CONSTRAINT chk_payment_ids CHECK (NOT (invoice_id IS NOT NULL AND bulkpayment_id IS NOT NULL))"),
+    ]
