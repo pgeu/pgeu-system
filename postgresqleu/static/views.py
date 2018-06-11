9fc195e6f1b2a5439f947072a37804a6f33c4d7f
Magnus Hagander <magnus@hagander.net>
2018-06-11 13:51:15 +0200

    Fix error handling for static fallback not found
    This should raise an actual 404 error and not throw a random exception
    caused by the django upgrade..

--- postgresqleu/static/views.py
+++ postgresqleu/static/views.py
@@ -1,18 +1,14 @@
 from django.http import HttpResponse, Http404
-from django.template import TemplateDoesNotExist, loader
+from django.template import loader
 
 # Fallback handler for URLs not matching anything else. Fall them
 # back to a static template. If that one is not found, send a 404
 # error.
 def static_fallback(request, url):
-	try:
-		# Disallow all URLs that back-step
-		if url.find('..') > -1:
-			raise TemplateDoesNotExist
-
-		t = loader.get_template('pages/%s.html' % url)
-		return HttpResponse(t.render())
-
-	except TemplateDoesNotExist:
+	# Disallow all URLs that back-step
+	if url.find('..') > -1:
 		raise Http404('Page not found')
+
+	t = loader.get_template('pages/%s.html' % url)
+	return HttpResponse(t.render())
 
