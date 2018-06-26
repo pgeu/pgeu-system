4ac6015c0a645d1ee967a47df6b74b415f03f2ce
Magnus Hagander <magnus@hagander.net>
2018-06-26 21:42:38 +0200

    Add widgets for pretty-printing json

--- postgresqleu/util/widgets.py
+++ postgresqleu/util/widgets.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.utils.safestring import mark_safe
 
+import json
+
 class RequiredFileUploadWidget(forms.FileInput):
 	def __init__(self, filename=None, attrs=None):
 		self.filename = filename
@@ -18,3 +20,20 @@ class RequiredFileUploadWidget(forms.FileInput):
 		output.append("Upload new file: ")
 		output.append(super(RequiredFileUploadWidget, self).render(name, value, attrs))
 		return mark_safe(u''.join(output))
+
+class PrettyPrintJsonWidget(forms.Textarea):
+	def render(self, name, value, attrs=None, renderer=None):
+		# This is mighty ugly -- we parse the json and then turn it back into json.
+		# Luckily this isn't called often :)
+		try:
+			value = json.dumps(json.loads(value), indent=2)
+		except ValueError:
+			# Don't try to do anything if it's not valid json
+			pass
+		t = super(PrettyPrintJsonWidget, self).render(name, value, attrs, renderer)
+		return t
+
+class AdminJsonWidget(PrettyPrintJsonWidget):
+	def render(self, name, value, attrs=None, renderer=None):
+		attrs['cols'] = 100
+		return super(AdminJsonWidget, self).render(name, value, attrs, renderer)
