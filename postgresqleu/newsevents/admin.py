c3476565377f91015ae5b225fc699ab3a0c1f3d1
Magnus Hagander <magnus@hagander.net>
2018-09-02 19:32:10 +0200

    Remove reference in admin

--- postgresqleu/newsevents/admin.py
+++ postgresqleu/newsevents/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from postgresqleu.newsevents.models import News, Event
+from postgresqleu.newsevents.models import News
 
-admin.site.register(Event)
 admin.site.register(News)
