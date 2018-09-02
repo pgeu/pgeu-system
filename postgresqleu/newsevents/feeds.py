96994a3ba3a248fe9a40cf717c46eeaf38254b79
Magnus Hagander <magnus@hagander.net>
2018-09-02 19:33:24 +0200

    More merge failure cleanups

--- postgresqleu/newsevents/feeds.py
+++ postgresqleu/newsevents/feeds.py
@@ -1,7 +1,7 @@
 from django.contrib.syndication.views import Feed
 from django.conf import settings
 
-from models import News, Event
+from models import News
 
 import datetime
 
@@ -16,20 +16,4 @@ class LatestNews(Feed):
 		
 	def item_link(self, news):
 		return "%s/news/%s" % (settings.SITEBASE, news.id)
-
-class LatestEvents(Feed):
-	title = "Events - %s" % settings.ORG_NAME
-	link = "%s/events/" % settings.SITEBASE
-	description = "The latest events from %s" % settings.ORG_NAME
-	description_template = "pieces/event_description.html"
-	
-	def items(self):
-		return Event.objects.filter(startdate__gte=datetime.datetime.today())[:10]
-		
-	def item_link(self, event):
-		return "%s/events/%s" % (settings.SITEBASE, event.id)
-
-	def item_pubdate(self, event):
-		# RSS feed needs a datetime object, not a date object
-		return datetime.datetime.fromordinal(event.startdate.toordinal())
 
