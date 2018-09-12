pgeu/pgeu-system

commit 69ae4f286fd6bf2305c7aeca69598b6bc88a2741

Fix imports of django urlresolves
Importing from django.core.urlresolvers is deprecated in favor of django.urls.

postgresqleu/confreg/backendviews.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.db import transaction
 from django import forms
-from django.core import urlresolvers
+from django.urls import reverse
 from django.core.exceptions import PermissionDenied
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.contrib.admin.utils import NestedObjects
@@ -155,8 +155,8 @@ def backend_process_form(request, urlname, formclass, id, cancel_url='../', save
 
 	if instance.id:
 		try:
-			adminurl = urlresolvers.reverse('admin:{0}_{1}_change'.format(instance._meta.app_label, instance._meta.model_name), args=(instance.id,))
-		except urlresolvers.NoReverseMatch:
+			adminurl = reverse('admin:{0}_{1}_change'.format(instance._meta.app_label, instance._meta.model_name), args=(instance.id,))
+		except NoReverseMatch:
 			adminurl = None
 	else:
 		adminurl = None

postgresqleu/adyen/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.utils.safestring import mark_safe
-from django.core import urlresolvers
+from django.urls import reverse
 
 from models import RawNotification, Notification
 from models import Report, TransactionStatus, AdyenLog, Refund
@@ -12,7 +12,7 @@ class RawNotificationAdmin(admin.ModelAdmin):
 	def notification_link(self, obj):
 		if obj.notification_set.exists():
 			n = obj.notification_set.all()[0]
-			url = urlresolvers.reverse("admin:adyen_notification_change", args=(n.id,))
+			url = reverse("admin:adyen_notification_change", args=(n.id,))
 			return mark_safe('<a href="%s">%s</a>' % (url, n))
 	notification_link.short_description = 'Notification'
 
@@ -23,7 +23,7 @@ class NotificationAdmin(admin.ModelAdmin):
 	search_fields = ('pspReference', 'merchantReference', 'reason', )
 
 	def rawnotification_link(self, obj):
-		url = urlresolvers.reverse('admin:adyen_rawnotification_change', args=(obj.rawnotification.id,))
+		url = reverse('admin:adyen_rawnotification_change', args=(obj.rawnotification.id,))
 		return mark_safe('<a href="%s">%s</a>' % (url, obj))
 	rawnotification_link.short_description = 'Rawnotification'
 
@@ -38,13 +38,13 @@ class TransactionStatusAdmin(admin.ModelAdmin):
 
 	def notification_link(self, obj):
 		if obj.notification:
-			url = urlresolvers.reverse("admin:adyen_notification_change", args=(obj.notification.id,))
+			url = reverse("admin:adyen_notification_change", args=(obj.notification.id,))
 			return mark_safe('<a href="%s">%s</a>' % (url, obj.notification))
 	notification_link.short_description = 'Notification'
 
 	def refund_link(self, obj):
 		if obj.refund:
-			url = urlresolvers.reverse("admin:adyen_refund_change", args=(obj.refund.id,))
+			url = reverse("admin:adyen_refund_change", args=(obj.refund.id,))
 			return mark_safe('%s at <a href="%s">%s</a>' % (obj.refund.refund_amount, url, obj.refund.receivedat))
 		else:
 			return "Not refunded"
@@ -57,13 +57,13 @@ class RefundAdmin(admin.ModelAdmin):
 
 	def notification_link(self, obj):
 		if obj.notification:
-			url = urlresolvers.reverse("admin:adyen_notification_change", args=(obj.notification.id,))
+			url = reverse("admin:adyen_notification_change", args=(obj.notification.id,))
 			return mark_safe('<a href="%s">%s</a>' % (url, obj.notification))
 	notification_link.short_description = 'Notification'
 
 	def transaction_link(self, obj):
 		if obj.transaction:
-			url = urlresolvers.reverse("admin:adyen_transactionstatus_change", args=(obj.transaction.id,))
+			url = reverse("admin:adyen_transactionstatus_change", args=(obj.transaction.id,))
 			return mark_safe('<a href="%s">%s</a>' % (url, obj.transaction.pspReference))
 	transaction_link.short_description = 'Transaction'
 

postgresqleu/adyen/management/commands/send_adyen_logreport.py
@@ -5,7 +5,7 @@
 #
 from django.core.management.base import BaseCommand
 from django.db import transaction
-from django.core import urlresolvers
+from django.urls import reverse
 from django.conf import settings
 
 from datetime import datetime, timedelta
@@ -45,7 +45,7 @@ class Command(BaseCommand):
 			sio = StringIO()
 			sio.write("The following notifications have not been confirmed in the Adyen integration.\nThese need to be manually processed and then flagged as confirmed!\n\nThis list only contains unconfirmed events older than 24 hours.\n\n\n")
 			for l in lines:
-				sio.write("%s: %s (%s%s)\n" % (l.eventDate, l.eventCode, settings.SITEBASE, urlresolvers.reverse('admin:adyen_notification_change', args=(l.id,))))
+				sio.write("%s: %s (%s%s)\n" % (l.eventDate, l.eventCode, settings.SITEBASE, reverse('admin:adyen_notification_change', args=(l.id,))))
 
 			send_simple_mail(settings.INVOICE_SENDER_EMAIL,
 							 settings.ADYEN_NOTIFICATION_RECEIVER,
@@ -62,7 +62,7 @@ class Command(BaseCommand):
 			sio.write("The following payments have been authorized, but not captured for more than %s days.\nThese probably need to be verified manually.\n\n\n" % UNSETTLED_THRESHOLD)
 
 			for l in lines:
-				sio.write("%s at %s: %s (%s%s)\n" % (l.pspReference, l.authorizedat, l.amount, settings.SITEBASE, urlresolvers.reverse('admin:adyen_transactionstatus_change', args=(l.id,))))
+				sio.write("%s at %s: %s (%s%s)\n" % (l.pspReference, l.authorizedat, l.amount, settings.SITEBASE, reverse('admin:adyen_transactionstatus_change', args=(l.id,))))
 
 			send_simple_mail(settings.INVOICE_SENDER_EMAIL,
 							 settings.ADYEN_NOTIFICATION_RECEIVER,

postgresqleu/adyen/util.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.core import urlresolvers
+from django.urls import reverse
 from django.db import transaction
 
 from datetime import datetime, date
@@ -292,7 +292,7 @@ def process_one_notification(notification):
 						 'Received unknown Adyen notification of type %s' % notification.eventCode,
 						 "An unknown Adyen notification of type %s has been received.\n\nYou'll need to go process this one manually:\n%s" % (
 							 notification.eventCode,
-							 urlresolvers.reverse('admin:adyen_notification_change', args=(notification.id,)),
+							 reverse('admin:adyen_notification_change', args=(notification.id,)),
 						 )
 		)
 		AdyenLog(pspReference=notification.pspReference, message='Received notification of unknown type %s' % notification.eventCode, error=True).save()

postgresqleu/confreg/admin.py
@@ -5,7 +5,7 @@ from django.forms import ValidationError
 from django.forms.utils import ErrorList
 from django.db.models.fields.files import ImageFieldFile
 from django.db.models import Count
-from django.core import urlresolvers
+from django.urls import reverse
 from django.utils.safestring import mark_safe
 from django.contrib.postgres.forms.ranges import RangeWidget
 
@@ -152,7 +152,7 @@ class ConferenceRegistrationAdmin(admin.ModelAdmin):
 
 	def invoice_link(self, inst):
 		if inst.invoice:
-			url = urlresolvers.reverse('admin:invoices_invoice_change', args=(inst.invoice.id,))
+			url = reverse('admin:invoices_invoice_change', args=(inst.invoice.id,))
 			return mark_safe('<a href="%s">%s</a>' % (url, inst.invoice))
 		else:
 			return ""
@@ -160,7 +160,7 @@ class ConferenceRegistrationAdmin(admin.ModelAdmin):
 
 	def bulkpayment_link(self, inst):
 		if inst.bulkpayment:
-			url = urlresolvers.reverse('admin:confreg_bulkpayment_change', args=(inst.bulkpayment.id,))
+			url = reverse('admin:confreg_bulkpayment_change', args=(inst.bulkpayment.id,))
 			return mark_safe('<a href="%s">%s</a>' % (url, inst.bulkpayment))
 		else:
 			return ""
@@ -469,7 +469,7 @@ class PrepaidVoucherAdmin(admin.ModelAdmin):
 	form = PrepaidVoucherAdminForm
 
 	def buyername(self, obj):
-		url = urlresolvers.reverse('admin:confreg_prepaidbatch_change', args=(obj.batch.pk,))
+		url = reverse('admin:confreg_prepaidbatch_change', args=(obj.batch.pk,))
 		return mark_safe('<a href="%s">%s</a>' % (url, obj.batch.buyername))
 	buyername.allow_tags = True
 

postgresqleu/confsponsor/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.http import HttpResponseRedirect
 from django.forms.models import BaseInlineFormSet
 from django.forms.utils import ErrorList
-from django.core import urlresolvers
+from django.urls import reverse
 from django.utils.safestring import mark_safe
 
 from selectable.forms.widgets import AutoCompleteSelectMultipleWidget
@@ -102,7 +102,7 @@ class SponsorAdmin(admin.ModelAdmin):
 
 	def invoice_link(self, inst):
 		if inst.invoice:
-			url = urlresolvers.reverse('admin:invoices_invoice_change', args=(inst.invoice.id,))
+			url = reverse('admin:invoices_invoice_change', args=(inst.invoice.id,))
 			return mark_safe('<a href="%s">%s</a>' % (url, inst.invoice))
 		else:
 			return ""

postgresqleu/trustlypayment/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.utils.safestring import mark_safe
-from django.core import urlresolvers
+from django.urls import reverse
 
 from models import TrustlyTransaction, TrustlyRawNotification, TrustlyNotification, TrustlyLog
 
@@ -14,7 +14,7 @@ class TrustlyRawNotificationAdmin(admin.ModelAdmin):
 	def notification_link(self, obj):
 		if obj.trustlynotification_set.exists():
 			n = obj.trustlynotification_set.all()[0]
-			url = urlresolvers.reverse('admin:trustlypayment_trustlynotification_change', args=(n.id,))
+			url = reverse('admin:trustlypayment_trustlynotification_change', args=(n.id,))
 			return mark_safe('<a href="{0}">{1}</a>'.format(url, n))
 	notification_link.short_description = 'Notification'
 
@@ -24,7 +24,7 @@ class TrustlyNotificationAdmin(admin.ModelAdmin):
 	exclude = ('rawnotification',)
 
 	def rawnotification_link(self, obj):
-		url = urlresolvers.reverse('admin:trustlypayment_trustlyrawnotification_change', args=(obj.rawnotification.id, ))
+		url = reverse('admin:trustlypayment_trustlyrawnotification_change', args=(obj.rawnotification.id, ))
 		return mark_safe('<a href="{0}">{1}</a>'.format(url, obj))
 	rawnotification_link.short_description='Rawnotification'
 

postgresqleu/trustlypayment/management/commands/send_trustly_logreport.py
@@ -5,7 +5,7 @@
 #
 from django.core.management.base import BaseCommand
 from django.db import transaction
-from django.core import urlresolvers
+from django.urls import reverse
 from django.conf import settings
 
 from datetime import datetime, timedelta
@@ -45,7 +45,7 @@ class Command(BaseCommand):
 			sio = StringIO()
 			sio.write("The following notifications have not been confirmed in the Trustly integration.\nThese need to be manually processed and then flagged as confirmed!\n\nThis list only contains unconfirmed events older than 24 hours.\n\n\n")
 			for l in lines:
-				sio.write("%s: %s (%s%s)\n" % (l.receivedat, l.method, settings.SITEBASE, urlresolvers.reverse('admin:trustlypayment_trustlynotification_change', args=(l.id,))))
+				sio.write("%s: %s (%s%s)\n" % (l.receivedat, l.method, settings.SITEBASE, reverse('admin:trustlypayment_trustlynotification_change', args=(l.id,))))
 
 			send_simple_mail(settings.INVOICE_SENDER_EMAIL,
 							 settings.TRUSTLY_NOTIFICATION_RECEIVER,
@@ -64,7 +64,7 @@ class Command(BaseCommand):
 			sio.write("The following payments have been authorized, but not finished for more than %s days.\nThese probably need to be verified manually.\n\n\n" % UNFINISHED_THRESHOLD)
 
 			for l in lines:
-				sio.write("%s at %s: %s (%s%s)\n" % (l.orderid, l.pendingat, l.amount, settings.SITEBASE, urlresolvers.reverse('admin:trustlypayment_trustlytransactionstatus_change', args=(l.id,))))
+				sio.write("%s at %s: %s (%s%s)\n" % (l.orderid, l.pendingat, l.amount, settings.SITEBASE, reverse('admin:trustlypayment_trustlytransactionstatus_change', args=(l.id,))))
 
 			send_simple_mail(settings.INVOICE_SENDER_EMAIL,
 							 settings.TRUSTLY_NOTIFICATION_RECEIVER,

commit e14f566209eada1f53eb41df924932c1de13e73d

Fix missing import
69ae4f286fd6bf2305c7aeca69598b6bc88a2741 broke the import of
NoReverseMatch when updating the url reversing.

postgresqleu/confreg/backendviews.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.db import transaction
 from django import forms
-from django.urls import reverse
+from django.urls import reverse, NoReverseMatch
 from django.core.exceptions import PermissionDenied
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.contrib.admin.utils import NestedObjects