7505ad8c437c511821d748525cdfeb82e415ebf3
Magnus Hagander <magnus@hagander.net>
2019-01-19 14:51:33 +0100

    Add template tag to get name of variable type

--- postgresqleu/confreg/templatetags/miscutil.py
+++ postgresqleu/confreg/templatetags/miscutil.py
@@ -6,3 +6,8 @@ register = template.Library()
 @register.filter(name='isboolean')
 def isboolean(value):
     return isinstance(value, bool)
+
+
+@register.filter(name='vartypename')
+def vartypename(value):
+    return type(value).__name__
