aca920306cb049a9c9ce0120577900a0a2405232
Magnus Hagander <magnus@hagander.net>
2019-02-01 15:53:43 +0100

    Update devsetup for py3

--- tools/devsetup/dev_setup.sh
+++ tools/devsetup/dev_setup.sh
@@ -20,7 +20,7 @@ psql -w "host=$PGH port=$PGP dbname=$PGD user=$PGU" -c "CREATE SCHEMA IF NOT EXI
 # Start from script directory to be safe!
 cd "${0%/*}"
 
-virtualenv --no-site-packages venv_dev
+virtualenv --no-site-packages --python=python3 venv_dev
 venv_dev/bin/pip install -r dev_requirements.txt
 
 cd ../..
