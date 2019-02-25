21316f7f9729d55cdcd0518bc946eb2b9716bad1
Andreas Scherbaum <ads@pgug.de>
2019-02-25 03:15:46 +0100

    Remove the old way the dev_setup script is asking for parameters Add commandline parameters instead Add a check for number of parameters (now required)
    Make the echo commands use ""

--- tools/devsetup/dev_setup.sh
+++ tools/devsetup/dev_setup.sh
@@ -2,19 +2,25 @@
 
 set -e
 
-echo "Host/path to postgres:"
-read PGH
-echo "Port:"
-read PGP
-echo "Database:"
-read PGD
-echo "User:"
-read PGU
+if [ "$#" -ne 4 ]; then
+    echo "Usage: $0 <hostname> <port> <database> <user>"
+    exit
+fi
 
-echo Verifying postgres connection...
+PGH=$1
+PGP=$2
+PGD=$3
+PGU=$4
+
+echo "Host/path to postgres: $PGH"
+echo "                 Port: $PGP"
+echo "             Database: $PGD"
+echo "                 User: $PGU"
+
+echo "Verifying postgres connection..."
 psql -w "host=$PGH port=$PGP dbname=$PGD user=$PGU" -c "SELECT 1" >/dev/null
 
-echo Verifying that pgcrypto is installed in the pgcrypto schema...
+echo "Verifying that pgcrypto is installed in the pgcrypto schema..."
 psql -w "host=$PGH port=$PGP dbname=$PGD user=$PGU" -c "CREATE SCHEMA IF NOT EXISTS pgcrypto; CREATE EXTENSION IF NOT EXISTS pgcrypto SCHEMA pgcrypto; SELECT pgcrypto.gen_random_uuid()"
 
 # Start from script directory to be safe!
@@ -48,14 +54,14 @@ ln -s tools/devsetup/venv_dev/bin/python .
 
 cat tools/devsetup/devserver-uwsgi.ini.tmpl | sed -e "s#%DJANGO%#$(pwd)/tools/devsetup/venv_dev#g" > devserver-uwsgi.ini
 
-echo
-echo Creating a django superuser, and setting password!
+echo ""
+echo "Creating a django superuser, and setting password!"
 ./python manage.py createsuperuser
 
-echo All ready to go. To start the development server, go to
+echo "All ready to go. To start the development server, go to"
 pwd
-echo and run:
-echo uwsgi --ini devserver-uwsgi.ini
-echo
-echo or for a slightly more limited version:
-echo ./python manage.py runserver
+echo "and run:"
+echo "uwsgi --ini devserver-uwsgi.ini"
+echo ""
+echo "or for a slightly more limited version:"
+echo "./python manage.py runserver"
