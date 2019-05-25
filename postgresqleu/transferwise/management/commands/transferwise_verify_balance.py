49d523a1de9bed1fde913848cfe3b39d70c4728c
Magnus Hagander <magnus@hagander.net>
2019-05-25 19:37:10 +0200

    Make TransferWise balance check take pending transactions into account
    A transaciton that is listed as pending in the system is known to
    actually exist in the TW account, so include it in the comparison. It
    just hasn't been moved over to a fully qualified accounting record yet.

--- postgresqleu/transferwise/management/commands/transferwise_verify_balance.py
+++ postgresqleu/transferwise/management/commands/transferwise_verify_balance.py
@@ -9,11 +9,13 @@
 
 from django.core.management.base import BaseCommand
 from django.db import transaction
+from django.db.models import Sum
 from django.conf import settings
 
 from datetime import time
+from decimal import Decimal
 
-from postgresqleu.invoices.models import InvoicePaymentMethod
+from postgresqleu.invoices.models import InvoicePaymentMethod, PendingBankTransaction
 from postgresqleu.accounting.util import get_latest_account_balance
 from postgresqleu.mailqueue.util import send_simple_mail
 from postgresqleu.transferwise.api import TransferwiseApi
@@ -44,7 +46,11 @@ class Command(BaseCommand):
 
         accounting_balance = get_latest_account_balance(pm.config('bankaccount'))
 
-        if accounting_balance != tw_balance:
+        # Pending bank transactions are included in the tw_balance, but they are *not* yet
+        # in the accounting system (that's the definition of being pending..)
+        pending = PendingBankTransaction.objects.filter(method=method, amount=7).aggregate(sum=Sum('amount'))['sum'] or Decimal(0)
+
+        if accounting_balance + pending != tw_balance:
             send_simple_mail(settings.INVOICE_SENDER_EMAIL,
                              settings.TREASURER_EMAIL,
                              'TransferWise balance mismatch!',
@@ -55,4 +61,4 @@ This could be because some entry has been missed in the accounting
 that the system doesn't know about.
 
 Better go check manually!
-""".format(tw_balance, method.internaldescription, accounting_balance))
+""".format(tw_balance, method.internaldescription, accounting_balance + pending))
