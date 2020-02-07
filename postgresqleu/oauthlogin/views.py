0c3af0709be0c9922e27eba15027bf0b76691aae
Magnus Hagander <magnus@hagander.net>
2020-02-07 14:39:28 +0100

    Properly pass along "next" parameter on oauth logins

--- postgresqleu/oauthlogin/views.py
+++ postgresqleu/oauthlogin/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render
 import django.contrib.auth.views as authviews
 from django.conf import settings
 
+from urllib.parse import quote
+
 
 def login(request):
     return render(request, 'oauthlogin/login.html', {
         'oauth_providers': [(k, v) for k, v in sorted(settings.OAUTH.items())],
+        'next': quote(request.GET.get('next', '')),
     })
 
 
