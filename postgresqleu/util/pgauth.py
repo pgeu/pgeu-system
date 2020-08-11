49a81ac34b9802bc43e6796e3890a389c341574e
Magnus Hagander <magnus@hagander.net>
2020-08-11 18:36:04 +0200

    Also mark auth_api wrapper as csrf exempt

--- postgresqleu/util/pgauth.py
+++ postgresqleu/util/pgauth.py
@@ -1,3 +1,5 @@
+from django.views.decorators.csrf import csrf_exempt
+
 import postgresqleu.auth
 
 from .decorators import global_login_exempt
@@ -6,5 +8,6 @@ from .decorators import global_login_exempt
 # Wrap the API endpoint and remove the requirement for global http
 # basic auth.
 @global_login_exempt
+@csrf_exempt
 def auth_api(*args, **kwargs):
     return postgresqleu.auth.auth_api(*args, **kwargs)
