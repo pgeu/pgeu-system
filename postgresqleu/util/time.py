8cbe450c0e3db453b4a99530b706c83e7dae7d2f
Magnus Hagander <magnus@hagander.net>
2021-01-25 15:28:19 +0100

    Add utility functions to format times in the global tz
    When going through templates the system takes care of it, but when
    manually rendering in the backend case needs to be taken, so add some
    utilities.

--- postgresqleu/util/time.py
+++ postgresqleu/util/time.py
@@ -1,4 +1,6 @@
 from django.utils import timezone
+from django.utils.timesince import timeuntil, timesince
+from django.utils.formats import date_format
 
 
 # Return a date represeting today in the timezone of the currently
@@ -12,3 +14,16 @@ def today_conference():
 # (making it suitable for everything that is not conference-related).
 def today_global():
     return timezone.localdate(timezone.now(), timezone.get_default_timezone())
+
+
+# Return a string representing time until or time since, depending
+def time_sinceoruntil(t):
+    if t >= timezone.now():
+        return "in {}".format(timeuntil(t))
+    else:
+        return "{} ago".format(timesince(t))
+
+
+# Format a datetime long string
+def datetime_string(t):
+    return date_format(timezone.localtime(t), 'DATETIME_FORMAT', use_l10n=False)
