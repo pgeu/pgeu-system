c213db91bab640dd3c31c3366a07c29862ed8d2f
Vik Fearing <vik@chouppes.com>
2022-05-16 14:41:00 +0200

    Teach json serialization to serialize Decimal values
    This is needed in newer versions of python (which may still have other
    issues, but fix this one as it showed up now).

--- postgresqleu/util/jsonutil.py
+++ postgresqleu/util/jsonutil.py
@@ -1,4 +1,5 @@
 from datetime import datetime, date
+from decimal import Decimal
 import json
 
 
@@ -6,6 +7,8 @@ class JsonSerializer(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, datetime) or isinstance(obj, date):
             return obj.isoformat()
+        if isinstance(obj, Decimal):
+            return str(obj)
         if hasattr(obj, 'json_included_attributes'):
             return dict([(k, getattr(obj, k)) for k in obj.json_included_attributes])
         return json.JSONEncoder.default(self, obj)
