5ae522a36c4a7d72409fad1bb5feb29f5b8974d2
Magnus Hagander <magnus@hagander.net>
2022-11-05 15:04:53 +0100

    Fix incorrectly copy/pasted error message

--- postgresqleu/util/messaging/sender.py
+++ postgresqleu/util/messaging/sender.py
@@ -88,7 +88,7 @@ def send_pending_posts(providers):
                 t.postids[id] = p.id
                 sentany = True
             else:
-                sys.stderr.write("Failed to post to {}: {}".format(p, err))
+                sys.stderr.write("Failed to repost on {}: {}\n".format(t.provider, msg))
                 err = True
         if sentany:
             numposts += 1
