682e93d07a31b5191a251dd0230ff73cb5f8f5ac
root <root@LAPTOP-V72J3PPQ.localdomain>
2023-01-21 17:37:34 +0530

    Add support to access documentation on help pages

--- postgresqleu/urls.py
+++ postgresqleu/urls.py
@@ -81,7 +81,7 @@ urlpatterns.extend([
 
     # Global admin
     url(r'^admin/$', postgresqleu.views.admin_dashboard),
-    url(r'^admin/docs/(?P<page>\w+/)?$', postgresqleu.util.docsviews.docspage),
+    url(r'^admin/docs/(?P<page>[\w.]+/)?$', postgresqleu.util.docsviews.docspage),
     url(r'^admin/mdpreview/$', postgresqleu.util.views.markdown_preview),
 
     # News
