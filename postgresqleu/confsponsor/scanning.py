4a01b43db5d8054fd4a421211d93460296f68f37
Daniel Gustafsson <daniel@yesql.se>
2023-01-23 21:46:44 +0100

    Fix typo in scanning warning dialog

--- postgresqleu/confsponsor/scanning.py
+++ postgresqleu/confsponsor/scanning.py
@@ -77,7 +77,7 @@ def sponsor_scanning(request, sponsorid):
                         scanner = SponsorScanner.objects.get(sponsor=sponsor, pk=rid)
                         n = scanner.scanner.fullname
                         if scanner.scanner.scanned_attendees.filter(sponsor=sponsor).exists():
-                            messages.warning(request, "Attende {0} has scanned badges already, cannot be removed".format(n))
+                            messages.warning(request, "Attendee {0} has scanned badges already, cannot be removed".format(n))
                         else:
                             scanner.delete()
                             messages.info(request, "Attendee {0} removed from scanning".format(n))
