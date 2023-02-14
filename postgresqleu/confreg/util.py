69538c0ac50e37c0a33c27fd3561829a1375d22e
Magnus Hagander <magnus@hagander.net>
2023-02-14 20:53:16 +0100

    Add misisng import

--- postgresqleu/confreg/util.py
+++ postgresqleu/confreg/util.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
 from django.conf import settings
 from django.core.exceptions import PermissionDenied
+from django.http import Http404
 from django.utils import timezone
 
 import json
