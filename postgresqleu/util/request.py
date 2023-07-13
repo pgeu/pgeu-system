e83cd018f524822ac3896fe2b0a3aef1b248315b
Magnus Hagander <magnus@hagander.net>
2023-07-13 17:24:13 +0200

    Add support for negative integers to get_int_or_error

--- postgresqleu/util/request.py
+++ postgresqleu/util/request.py
@@ -1,14 +1,20 @@
 from django.http import Http404
 
 
-def get_int_or_error(reqmap, paramname, default=None):
+def get_int_or_error(reqmap, paramname, default=None, allow_negative=False):
     if paramname not in reqmap:
         if default:
             return default
         raise Http404("Parameter {} missing".format(paramname))
 
     p = reqmap.get(paramname)
+    if allow_negative and p.startswith('-'):
+        p = p[1:]
+        negative = -1
+    else:
+        negative = 1
+
     if not p.isnumeric():
         raise Http404("Parameter {} is not an integer".format(paramname))
 
-    return int(p)
+    return int(p) * negative
