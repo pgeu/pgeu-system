76525c1d65d1f10f31c70095258cdd5bd4757eec
Magnus Hagander <magnus@hagander.net>
2023-10-16 12:06:48 +0200

    Add wrapper class to render linebreaks in backend lists
    By wrapping a string in this class, \n will be turned into <br> in the
    *list*. This is normally not wanted as it can mess with formatting which
    is why we don't do it by default, but this allows an override of the
    default behaviour.

--- postgresqleu/util/markup.py
+++ postgresqleu/util/markup.py
@@ -5,3 +5,8 @@ import markdown
 # as for now anybody entering markdown is considered trusted.
 def pgmarkdown(value):
     return markdown.markdown(value, extensions=['tables', ])
+
+
+# Wrapper class for a string that we want linebreaks to be visible in
+class LineBreakString(str):
+    pass
