878d1540d083b6ab611d985a6228ec9b5921a893
Magnus Hagander <magnus@hagander.net>
2024-04-16 23:01:52 +0200

    Fix hashval NULLability
    Needs to allow NULL on INSERT, which will then get overwritten by the
    trigger.

--- postgresqleu/util/models.py
+++ postgresqleu/util/models.py
@@ -6,7 +6,7 @@ class Storage(models.Model):
     key = models.CharField(max_length=16, null=False, blank=False)
     storageid = models.IntegerField(null=False, blank=False)
     data = models.BinaryField(null=False, blank=False)
-    hashval = models.BinaryField(null=False, blank=False)
+    hashval = models.BinaryField(null=True, blank=False)
 
     class Meta:
         unique_together = (
