pgeu/pgeu-system

commit 52fa57dc9b7604a5739a65f1a2129a4774dd4851

Add ability to schedule attendee emails for the future
This includes both direct attendee emails, and also those that are for
example sent to speakers of a session. This also removes the support for
sending such emails *without* storing them as attendee emails, as that
wouldn't leave us with a way to track what's been sent and what hasn't.

Fixes #90

postgresqleu/confreg/mail.py
@@ -3,8 +3,11 @@ from django.utils.html import escape
 from django.db import transaction
 from django.http import Http404, HttpResponseRedirect
 from django.contrib import messages
+from django.utils import timezone
+
 
 from postgresqleu.util.db import exec_to_dict, exec_to_scalar, exec_no_result
+from postgresqleu.scheduler.util import trigger_immediate_job_run
 
 from .util import send_conference_mail
 from .backendforms import BackendSendEmailForm
@@ -37,7 +40,6 @@ def attendee_email_form(request, conference, query=None, breadcrumbs=[], templat
         '_from': '{0} <{1}>'.format(conference.conferencename, conference.contactaddr),
         'recipients': escape(", ".join(['{0} <{1}>'.format(x['fullname'], x['email']) for x in recipients])),
         'idlist': ",".join(map(str, idlist)),
-        'storeonregpage': True,
     }
 
     if request.method == 'POST':
@@ -46,42 +48,31 @@ def attendee_email_form(request, conference, query=None, breadcrumbs=[], templat
         form = BackendSendEmailForm(conference, data=p, initial=initial)
         if form.is_valid():
             with transaction.atomic():
-                if form.cleaned_data['storeonregpage']:
-                    mailid = exec_to_scalar("INSERT INTO confreg_attendeemail (conference_id, sentat, subject, message, tocheckin, tovolunteers) VALUES (%(confid)s, CURRENT_TIMESTAMP, %(subject)s, %(message)s, false, false) RETURNING id", {
-                        'confid': conference.id,
-                        'subject': form.cleaned_data['subject'],
-                        'message': form.cleaned_data['message'],
-                    })
+                mailid = exec_to_scalar("INSERT INTO confreg_attendeemail (conference_id, sent, sentat, subject, message, tocheckin, tovolunteers) VALUES (%(confid)s, false, %(sentat)s, %(subject)s, %(message)s, false, false) RETURNING id", {
+                    'confid': conference.id,
+                    'sentat': form.cleaned_data['sendat'],
+                    'subject': form.cleaned_data['subject'],
+                    'message': form.cleaned_data['message'],
+                })
                 for r in recipients:
-                    send_conference_mail(conference,
-                                         r['email'],
-                                         form.cleaned_data['subject'],
-                                         template,
-                                         {
-                                             'body': form.cleaned_data['message'],
-                                             'linkback': form.cleaned_data['storeonregpage'],
-                                         } | extracontext,
-                                         receivername=r['fullname'],
-                    )
-
-                    if form.cleaned_data['storeonregpage']:
-                        if r['regid']:
-                            # Existing registration, so attach directly to attendee
-                            exec_no_result("INSERT INTO confreg_attendeemail_registrations (attendeemail_id, conferenceregistration_id) VALUES (%(mailid)s, %(reg)s)", {
-                                'mailid': mailid,
-                                'reg': r['regid'],
-                            })
-                        else:
-                            # No existing registration, so queue it up in case the attendee
-                            # might register later. We have the userid...
-                            exec_no_result("INSERT INTO confreg_attendeemail_pending_regs (attendeemail_id, user_id) VALUES (%(mailid)s, %(userid)s)", {
-                                'mailid': mailid,
-                                'userid': r['user_id'],
-                            })
-                if form.cleaned_data['storeonregpage']:
-                    messages.info(request, "Email sent to %s attendees, and added to their registration pages when possible" % len(recipients))
+                    if r['regid']:
+                        # Existing registration, so attach directly to attendee
+                        exec_no_result("INSERT INTO confreg_attendeemail_registrations (attendeemail_id, conferenceregistration_id) VALUES (%(mailid)s, %(reg)s)", {
+                            'mailid': mailid,
+                            'reg': r['regid'],
+                        })
+                    else:
+                        # No existing registration, so queue it up in case the attendee
+                        # might register later. We have the userid...
+                        exec_no_result("INSERT INTO confreg_attendeemail_pending_regs (attendeemail_id, user_id) VALUES (%(mailid)s, %(userid)s)", {
+                            'mailid': mailid,
+                            'userid': r['user_id'],
+                        })
+                if form.cleaned_data['sendat'] > timezone.now():
+                    messages.info(request, "Email scheduled for later sending to attendees")
                 else:
-                    messages.info(request, "Email sent to %s attendees" % len(recipients))
+                    trigger_immediate_job_run('confreg_send_emails')
+                    messages.info(request, "Email sent to attendees, and added to their registration pages")
 
             return HttpResponseRedirect('../')
     else:

postgresqleu/confreg/views.py
@@ -79,6 +79,7 @@ from postgresqleu.util.db import exec_to_dict, exec_to_grouped_dict, exec_to_key
 from postgresqleu.util.db import exec_no_result, exec_to_list, exec_to_scalar, conditional_exec_to_scalar
 from postgresqleu.util.db import ensure_conference_timezone
 from postgresqleu.util.qr import generate_base64_qr
+from postgresqleu.scheduler.util import trigger_immediate_job_run
 
 from decimal import Decimal
 from operator import itemgetter
@@ -110,7 +111,7 @@ def _get_registration_signups(conference, reg):
 # Should then be extended with whatever other requirements there are to limit
 # what's actually returned.
 def _attendeemail_queryset(conference, reg):
-    return AttendeeMail.objects.select_related('conference').filter(conference=conference).extra(where=["""
+    return AttendeeMail.objects.select_related('conference').filter(conference=conference, sent=True).extra(where=["""
  EXISTS (SELECT 1 FROM confreg_attendeemail_regclasses rc WHERE rc.attendeemail_id=confreg_attendeemail.id AND registrationclass_id=%s)
 OR
  EXISTS (SELECT 1 FROM confreg_attendeemail_addopts ao INNER JOIN confreg_conferenceregistration_additionaloptions rao ON rao.conferenceadditionaloption_id=ao.conferenceadditionaloption_id WHERE ao.attendeemail_id=confreg_attendeemail.id AND rao.conferenceregistration_id=%s)
@@ -4334,6 +4335,7 @@ def admin_attendeemail(request, urlname):
                                message=form.data['message'],
                                tovolunteers='tovolunteers' in form.data,
                                tocheckin='tocheckin' in form.data,
+                               sentat=form.cleaned_data['sentat'],
             )
             msg.save()
             for rc in form.data.getlist('regclasses'):
@@ -4342,27 +4344,12 @@ def admin_attendeemail(request, urlname):
                 msg.addopts.add(ao)
             msg.save()
 
-            # Now also send the email out to the currently registered attendees
-            attendees = set(ConferenceRegistration.objects.filter(conference=conference, payconfirmedat__isnull=False, canceledat__isnull=True, regtype__regclass__in=form.data.getlist('regclasses')))
-            if form.data.getlist('addopts'):
-                attendees.update(ConferenceRegistration.objects.filter(conference=conference, payconfirmedat__isnull=False, canceledat__isnull=True, additionaloptions__in=form.data.getlist('addopts')))
-            if msg.tovolunteers:
-                attendees.update(conference.volunteers.all())
-            if msg.tocheckin:
-                attendees.update(conference.checkinprocessors.all())
+            if msg.sentat > timezone.now():
+                messages.info(request, "Email scheduled for later sending to attendees")
+            else:
+                trigger_immediate_job_run('confreg_send_emails')
+                messages.info(request, "Email sent to attendees, and added to their registration pages")
 
-            for a in attendees:
-                send_conference_mail(conference,
-                                     a.email,
-                                     msg.subject,
-                                     'confreg/mail/attendee_mail.txt',
-                                     {
-                                         'body': msg.message,
-                                         'linkback': True,
-                                     },
-                                     receivername=a.fullname,
-                )
-            messages.info(request, "Email sent to %s attendees, and added to their registration pages" % len(attendees))
             return HttpResponseRedirect(".")
     else:
         form = AttendeeMailForm(conference)

postgresqleu/confreg/backendforms.py
@@ -6,6 +6,7 @@ from django.contrib import messages
 from django.contrib.auth.models import User
 import django.forms
 import django.forms.widgets
+from django.utils import timezone
 from django.utils.safestring import mark_safe
 from django.utils.html import escape
 from django.conf import settings
@@ -1893,10 +1894,9 @@ class BulkPaymentRefundForm(django.forms.Form):
 #
 class BackendSendEmailForm(django.forms.Form):
     _from = django.forms.CharField(max_length=100, disabled=True, label="From")
+    sendat = django.forms.DateTimeField(required=True, initial=timezone.now)
     subject = django.forms.CharField(max_length=128, required=True)
     recipients = django.forms.Field(widget=StaticTextWidget, required=False)
-    storeonregpage = django.forms.BooleanField(label="Store on registration page", required=False,
-                                               help_text="If checked, store in db and show to attendees later. If not checked, one-off email is sent.")
     message = django.forms.CharField(widget=EmailTextWidget, required=True)
     idlist = django.forms.CharField(widget=django.forms.HiddenInput, required=True)
     confirm = django.forms.BooleanField(label="Confirm", required=False)

postgresqleu/confreg/forms.py
@@ -705,7 +705,7 @@ class AttendeeMailForm(forms.ModelForm):
 
     class Meta:
         model = AttendeeMail
-        fields = ('regclasses', 'addopts', 'tovolunteers', 'tocheckin', 'subject', 'message')
+        fields = ('regclasses', 'addopts', 'tovolunteers', 'tocheckin', 'sentat', 'subject', 'message')
         widgets = {
             'message': EmailTextWidget,
         }

postgresqleu/confreg/management/commands/confreg_send_emails.py
@@ -1,0 +1,53 @@
+# Send queued attendee emails.
+#
+
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+from django.db import transaction
+from django.db.models import Q
+from django.conf import settings
+
+from datetime import timedelta
+
+from postgresqleu.confreg.models import AttendeeMail, ConferenceRegistration
+from postgresqleu.confreg.util import send_conference_mail
+
+
+class Command(BaseCommand):
+    help = 'Send attendee emails'
+
+    class ScheduledJob:
+        scheduled_interval = timedelta(minutes=5)
+        internal = True
+
+        @classmethod
+        def should_run(self):
+            return AttendeeMail.objects.filter(sentat__lte=timezone.now(), sent=False).exists()
+
+    @transaction.atomic
+    def handle(self, *args, **options):
+        for msg in AttendeeMail.objects.filter(sentat__lte=timezone.now(), sent=False):
+            attendees = set(ConferenceRegistration.objects.filter(conference=msg.conference, payconfirmedat__isnull=False, canceledat__isnull=True, regtype__regclass__attendeemail=msg))
+            attendees.update(ConferenceRegistration.objects.filter(conference=msg.conference, payconfirmedat__isnull=False, canceledat__isnull=True, additionaloptions__attendeemail=msg))
+            if msg.tovolunteers:
+                attendees.update(msg.conference.volunteers.all())
+            if msg.tocheckin:
+                attendees.update(msg.conference.checkinprocessors.all())
+
+            # Pending registrations don't have a ConferenceRegistration object, so we need to extract just the parts we need.
+            recipients = [(a.fullname, a.email) for a in attendees]
+            recipients.extend([('{} {}'.format(u.first_name, u.last_name), u.email) for u in msg.pending_regs.all()])
+
+            for fullname, email in recipients:
+                send_conference_mail(msg.conference,
+                                     email,
+                                     msg.subject,
+                                     'confreg/mail/attendee_mail.txt',
+                                     {
+                                         'body': msg.message,
+                                         'linkback': True,
+                                     },
+                                     receivername=fullname,
+                )
+            msg.sent = True
+            msg.save(update_fields=['sent'])

postgresqleu/confreg/migrations/0001_initial.py
@@ -8,6 +8,7 @@ from postgresqleu.util.fields import LowercaseEmailField
 import postgresqleu.confreg.dbimage
 from django.conf import settings
 import django.core.validators
+from django.utils import timezone
 
 
 class Migration(migrations.Migration):
@@ -21,7 +22,7 @@ class Migration(migrations.Migration):
             name='AttendeeMail',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('sentat', models.DateTimeField(auto_now_add=True)),
+                ('sentat', models.DateTimeField(default=timezone.now, verbose_name="Send at")),
                 ('subject', models.CharField(max_length=100)),
                 ('message', models.TextField(max_length=8000)),
             ],

postgresqleu/confreg/migrations/0113_attendeemail_sent.py
@@ -1,0 +1,28 @@
+# Generated by Django 4.2.11 on 2024-05-14 14:22
+
+from django.db import migrations, models
+import django.utils.timezone
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('confreg', '0112_unique_regtype'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='attendeemail',
+            name='sent',
+            field=models.BooleanField(default=True),
+        ),
+        migrations.AlterField(
+            model_name='attendeemail',
+            name='sent',
+            field=models.BooleanField(default=False),
+        ),
+        migrations.AddIndex(
+            model_name='attendeemail',
+            index=models.Index(condition=models.Q(('sent', False)), fields=['sentat'], name='confreg_attendeemail_unsent'),
+        ),
+    ]

postgresqleu/confreg/models.py
@@ -1414,7 +1414,8 @@ class AttendeeMail(models.Model):
     tovolunteers = models.BooleanField(null=False, blank=False, default=False, verbose_name="To volunteers")
     tocheckin = models.BooleanField(null=False, blank=False, default=False, verbose_name="To check-in processors")
     addopts = models.ManyToManyField(ConferenceAdditionalOption, blank=True, verbose_name="Attendees with options")
-    sentat = models.DateTimeField(null=False, blank=False, auto_now_add=True)
+    sentat = models.DateTimeField(null=False, blank=False, default=timezone.now, verbose_name="Send at")
+    sent = models.BooleanField(null=False, blank=False, default=False)
     subject = models.CharField(max_length=100, null=False, blank=False)
     message = models.TextField(max_length=8000, null=False, blank=False)
 
@@ -1423,6 +1424,13 @@ class AttendeeMail(models.Model):
 
     class Meta:
         ordering = ('-sentat', )
+        indexes = [
+            models.Index(name="confreg_attendeemail_unsent", fields=['sentat'], condition=Q(sent=False)),
+        ]
+
+    @property
+    def future(self):
+        return self.sentat > timezone.now()
 
 
 class PendingAdditionalOrder(models.Model):

commit c2f082a7574050419ad26f59c812f73f36234498

Remove support for templtaes in attendee emails
This was accidentally broken in 52fa57dc, and went mostly unnoticed. It
was only used by signup emails which now ended up having just one link
instead of two, so it seems fine for now. We'll eventually want to add
more smarness to the templates around this, but that can't be done
without a bigger surgery so for now just remove it.

postgresqleu/confreg/mail.py
@@ -13,7 +13,7 @@ from .util import send_conference_mail
 from .backendforms import BackendSendEmailForm
 
 
-def attendee_email_form(request, conference, query=None, breadcrumbs=[], template='confreg/mail/attendee_mail.txt', extracontext={}, strings=False):
+def attendee_email_form(request, conference, query=None, breadcrumbs=[], extracontext={}, strings=False):
     if request.method == 'POST':
         if strings:
             idlist = request.POST['idlist'].split(',')

postgresqleu/confwiki/views.py
@@ -602,7 +602,6 @@ def signup_admin_sendmail(request, urlname, signupid):
                 ('../../', 'Signups'),
                 ('../', signup.title),
             ],
-            template='confwiki/mail/signup_mail.txt',
             extracontext={'signup': signup},
             query=_get_query,
             strings=True,