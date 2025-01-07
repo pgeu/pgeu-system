dcc5e113fbae8395759013874e5f300dc3d71d52
Magnus Hagander <magnus@hagander.net>
2025-01-07 11:08:23 +0100

    Fix missing quotes in version check

--- postgresqleu/util/versionutil.py
+++ postgresqleu/util/versionutil.py
@@ -3,7 +3,7 @@ import jwt
 
 
 def decode_unverified_jwt(j):
-    if jwt.__version__ > 2:
+    if jwt.__version__ > "2":
         return jwt.decode(j, options={'verify_signature': False})
     else:
         return jwt.decode(j, verify=False)
