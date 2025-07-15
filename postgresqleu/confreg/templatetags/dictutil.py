72782ca7d85911157dffb70569ece09c219ff7e0
Magnus Hagander <magnus@hagander.net>
2025-07-15 12:24:04 +0200

    Fix django filter for handling multiple parameters
    Django template filters can't take more than one parameter, so we have
    to split it on comma ourselves. This filter was never used with more
    than one parameter, even though it claimed to support it.

--- postgresqleu/confreg/templatetags/dictutil.py
+++ postgresqleu/confreg/templatetags/dictutil.py
@@ -13,8 +13,13 @@ def arrayelement(value, key):
     return value[key]
 
 
+# Use comma to select a different separator
 @register.filter
-def join_dictkeys(list_to_join, attrname, separator=', '):
+def join_dictkeys(list_to_join, attrname):
     if not list_to_join:
         return ''
+    if ',' in attrname:
+        attrname, separator = attrname.split(',', 1)
+    else:
+        separator = ', '
     return separator.join(str(item[attrname]) for item in list_to_join)
