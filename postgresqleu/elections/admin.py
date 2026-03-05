0955e2b3fb0d4f3edc7a26b71f4062c40910ac89
Magnus Hagander <magnus@hagander.net>
2026-03-05 23:33:32 +0100

    Remove election vote admin pages
    This really was debug level code that got left around for far too long.

--- postgresqleu/elections/admin.py
+++ postgresqleu/elections/admin.py
@@ -4,22 +4,6 @@ from django.forms import ValidationError, ModelForm
 from .models import Vote, Election, Candidate
 
 
-class VoteAdminForm(ModelForm):
-    class Meta:
-        model = Vote
-        exclude = []
-
-    def clean(self):
-        raise ValidationError("You really shouldn't edit votes! If you *really* need to fix something broken, do it in the db")
-
-
-class VoteAdmin(admin.ModelAdmin):
-    list_display = ('election', 'voter', 'candidate', 'score')
-    autocomplete_fields = ('voter', )
-    ordering = ['election', ]
-    form = VoteAdminForm
-
-
 class ElectionAdmin(admin.ModelAdmin):
     list_display = ['name', 'startdate', 'enddate', ]
     ordering = ['-startdate', ]
@@ -33,4 +17,3 @@ class CandidateAdmin(admin.ModelAdmin):
 
 admin.site.register(Election, ElectionAdmin)
 admin.site.register(Candidate, CandidateAdmin)
-admin.site.register(Vote, VoteAdmin)
